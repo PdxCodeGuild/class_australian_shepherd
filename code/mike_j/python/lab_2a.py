9e8e804af6134f46be9524930da95626414427ea
mjack9615 <mjack9615@gmail.com>
2022-08-02 14:00:25 -0700

    mike_j-python-lab_2a

---
+++ code/mike_j/python/lab_2a.py
@@ -0,0 +1,18 @@
+'''Friends, Romans, countrymen, lend me your ears;
+I come to bury Caesar, not to praise him.
+The evil that men do lives after them;
+The good is oft interred with their bones;
+So let it be with Caesar. '''
+
+countrymen=input("Type plural noun: ")
+ears=input("Type plural noun: ")
+bury=input("Type verb: ")
+men=input("Type plural noun: ")
+good=input("Type singular noun: ")
+bones=input("Type plural noun: ")
+
+print(f'''Friends, Romans, {countrymen}, lend me your {ears}
+I come to {bury} Caesar, not to praise him.
+The evil that {men} do lives after them
+The {good} is oft interred with their {bones}
+So let it be with Caesar.''')
