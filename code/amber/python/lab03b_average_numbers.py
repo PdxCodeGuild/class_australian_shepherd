5e1e94f4c1cfad5fe3fcdc38591d498b8af66364
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-02 21:31:18 -0700

    Add variables for nums list, sum, length, and average.

--- code/amber/python/lab03b_average_numbers.py
+++ code/amber/python/lab03b_average_numbers.py
@@ -1,1 +1,19 @@
 #AVERAGE NUMBERS
+
+nums = [5, 0, 8, 3, 4, 1, 6]
+
+for num in nums:
+  num += num
+
+sum = num
+
+length = len(nums)
+
+average = sum / length
+
+for i in range(length):
+  print(nums[i])
+
+print(sum)
+
+print(average)
