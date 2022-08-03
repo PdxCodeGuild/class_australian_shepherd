db36aec19d6a0f503dde5646d597367411dab202
bean daddy <ignacioramirez@bean-daddy-2.local>
2022-08-02 19:16:38 -0700

    order up

---
+++ code/ignacio/lab_2a.py
@@ -0,0 +1,10 @@
+print('Welcome to my mad libs!')
+
+customer = input('Please enter a two digit number: ')
+order = input('Enter favorite food: ')
+snack = input('Enter favorite snack: ')
+taste = input('Enter an adjictive: ')
+
+result = f'Order {customer}! Your {order} topped with {snack} is ready! Would you like that {taste}?'
+
+print(result)
