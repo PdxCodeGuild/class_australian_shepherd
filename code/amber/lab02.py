1290a93670ba28a30b90364a626c28f3d742d31d
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-02 19:13:17 -0700

    Add madlib f string variable and print statement to complete version 1.

--- code/amber/lab02.py
+++ code/amber/lab02.py
@@ -4,10 +4,14 @@ import random
 
 # "Everyone should have their minds blown at least once a day." - Neil deGrasse Tyson
 
-person = input("enter a type of person/people (plural): ")
+person = input("enter a type of person/people (plural): ").title()
 body_part = input("Enter a body part (plural): ")
 action = input("Enter an action word (past tense): ")
 unit_of_time = input("Enter a unit of time: ")
-first_name = input("Enter a first name: ")
-middle_name = input("Enter a middle name: ")
-last_name = input("Enter a last name: ")
+first_name = input("Enter a first name: ").title()
+middle_name = input("Enter a middle name: ").title()
+last_name = input("Enter a last name: ").title()
+
+madlib = f'"{person} should have their {body_part} {action} at least once a {unit_of_time}." - {first_name} {middle_name} {last_name}'
+
+print(madlib)
