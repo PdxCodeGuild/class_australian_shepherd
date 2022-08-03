5181ab383f4fb487cdffc8acbaebfde0a8978f9f
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-02 19:07:16 -0700

    Create lab02.py file with initial madlib input variables.

---
+++ code/amber/lab02.py
@@ -0,0 +1,13 @@
+# MADLIBS
+
+import random
+
+# "Everyone should have their minds blown at least once a day." - Neil deGrasse Tyson
+
+person = input("enter a type of person/people (plural): ")
+body_part = input("Enter a body part (plural): ")
+action = input("Enter an action word (past tense): ")
+unit_of_time = input("Enter a unit of time: ")
+first_name = input("Enter a first name: ")
+middle_name = input("Enter a middle name: ")
+last_name = input("Enter a last name: ")
