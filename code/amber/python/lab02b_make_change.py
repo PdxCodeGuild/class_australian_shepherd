da40a216a45a500c2a67f24113bb791669979997
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-03 23:41:50 -0700

    Add make change fu nction to complete v.1.

--- code/amber/python/lab02b_make_change.py
+++ code/amber/python/lab02b_make_change.py
@@ -1,7 +1,28 @@
 # MAKE CHANGE
 
+dollars_in = 0.67
+
+def make_change(money):
+  pennies = int(money * 100)
+  quarters = pennies // 25
+  dimes = (pennies - quarters * 25) // 10
+  nickels = ((pennies - quarters * 25) - (dimes * 10)) // 5
+  end_pennies = ((pennies - quarters * 25) - (dimes * 10)) % 5
+  return f'{quarters} quarters, {dimes} dimes, {nickels} nickels, {end_pennies} pennies.'
+
+print(make_change(dollars_in))
+'''
 dollars_in = 1.36
 
-pennies = dollars_in * 100
+pennies = int(dollars_in * 100)
+quarters = pennies // 25
+dimes = (pennies - quarters * 25) // 10
+nickels = ((pennies - quarters * 25) - (dimes * 10)) // 5
+end_pennies = ((pennies - quarters * 25) - (dimes * 10)) % 5
 
-print(pennies)
+print(f'starting pennies = {pennies}')
+print(f'quarters = {quarters}')
+print(f'dimes = {dimes}')
+print(f'nickles = {nickels}')
+print(f'end pennies = {end_pennies}')
+'''
