84638983ac5551c95a6196d526665befadc294bf
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-04 15:53:43 -0700

    Add input, list variable, reverse list for loop, reverse string variable and conditional print statement for palindrome checker.

--- code/amber/python/lab05_palindromes.py
+++ code/amber/python/lab05_palindromes.py
@@ -1,1 +1,20 @@
 # PALINDROMES
+
+import string
+
+word_input = input("Enter a word to check if its a palindrome: ")
+
+word_list = list(word_input.lower())
+
+list_reversed = []
+
+for i in range(len(word_list)):
+  list_reversed.append(word_list.pop())
+
+backward = ('').join(list_reversed)
+
+if word_input == backward:
+  print(f'{word_input} is a palindrome.')
+
+else:
+  print(f'{word_input} is NOT a palindrome.')
