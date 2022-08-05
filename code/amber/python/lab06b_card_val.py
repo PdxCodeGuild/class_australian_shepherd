c9f73bfc5262aced36cd7194c9c5d6868d17f201
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-05 07:29:47 -0700

    BUGFIX: adjusted split sum and sum digit variables to return the correct data types.

--- code/amber/python/lab06b_card_val.py
+++ code/amber/python/lab06b_card_val.py
@@ -1,33 +1,66 @@
 # CREDIT CARD VALIDATION
 
-user_cc = '4556737586899855'
+user_cc = input('Please enter your credit card number: ')
 
 def cc_validator(cc_num):
+
   cc_num_list = list(cc_num)
   for i in range(len(cc_num_list)):
     cc_num_list[i] = int(cc_num_list[i])
 
+  print(cc_num_list)
+
+
   check_digit = []
   check_digit.append(cc_num_list.pop())
+
+  print(check_digit)
+
 
   reversed_digits = []
   for i in range(len(cc_num_list)):
     reversed_digits.append(cc_num_list.pop())
 
+  print(reversed_digits)
+
   for i in range(0, len(reversed_digits), 2):
     reversed_digits[i] *= 2
+
+  print(reversed_digits)
+
 
   for i in range(len(reversed_digits)):
     if reversed_digits[i] > 9:
       reversed_digits[i] -= 9
 
+
+  print(reversed_digits)
+
   sum = 0
   for i in range(len(reversed_digits)):
     sum += reversed_digits[i]
 
-  split_sum = list(str(sum))
+  print(sum)
 
-  sum_digit = int(split_sum[-1])
+
+  split_sum = list(str(sum))
+  for i in range(len(split_sum)):
+    split_sum[i] = int(split_sum[i])
+
+  print(split_sum)
+
+
+  sum_digit = []
+
+  sum_digit.append(split_sum.pop())
+
+
+  print(sum_digit, type(sum_digit))
+
 
   if sum_digit == check_digit:
     return True
+
+
+
+print(cc_validator(user_cc))
