bb0bb95cae09f8fbf0928db4c0094c80a7d04c5b
matthew chimenti <matthewosu@gmail.com>
2022-08-05 19:08:27 -0700

    updated wheel and notes added

---
+++ code/matt_instructor/python/wheel_of_terror/questions.py
@@ -0,0 +1,82 @@
+terms = [
+    {
+        "topic": "Indentation",
+        "definition": "Indentation refers to the spaces at the beginning of a code line"
+    },
+    {
+        "topic": "Comments",
+        "definition": "Comments are code lines that will not be executed"
+    },
+    {
+        "topic": "Tuple",
+        "definition": "A tuple is an ordered, and unchangeable, collection"
+    },
+    {
+        "topic": "Sequence Length",
+        "definition": "The number of elements in the peice of data"
+    },
+    {
+        "topic": "Dictionary",
+        "definition": "A dictionary is an unordered, and changeable, collection"
+    },
+    {
+        "topic": "Conditional Statement",
+        "definition": "Is a set of rules performed if a certain condition is met. It is sometimes referred to as an If-Then statement, because IF a condition is met, THEN an action is performed."
+    },
+    {
+        "topic": "Break keyword",
+        "definition": "How to break a for loop"
+    },
+    {
+        "topic": "Parameter ",
+        "definition": "Parameters are variables that are defined in the function definition. They are assigned the values which were passed as arguments when the function was called, elsewhere in the code."
+    },
+    {
+        "topic": "Arguments",
+        "definition": "Information can be passed into functions."
+    },
+    {
+        "topic": "Code Block",
+        "definition": "A code block is a piece of Python program text that can be executed as a unit, such as a module, a class definition or a function body"
+    },
+    {
+        "topic": "Scope",
+        "definition": "The scope defines the accessibility of the python object."
+    },
+    {
+        "topic": "Function",
+        "definition": "A function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function. A function can return data as a result."
+    },
+    {
+        "topic": "Method",
+        "definition": "A method is a function that belongs to an object. (In Python, the term method is not unique to class instances: other object types can have methods as well. For example, list objects have methods called append, insert, remove, sort, and so on."
+    },
+    {
+        "topic": "Boolean",
+        "definition": "The Python Boolean type is one of Python's built-in data types. It's used to represent the truth value of an expression."
+    },
+    {
+        "topic": "Assignment Operator",
+        "definition": "Assignment operators are used in Python to assign values to variables. a = 5 is a simple assignment operator that assigns the value 5 on the right to the variable a on the left."
+    },
+    {
+        "topic": "Module",
+        "definition": "In Python, Modules are simply files with the . py extension containing Python code that can be imported inside another Python Program"
+    },
+    {
+        "topic": "Variable",
+        "definition": "A Python variable is a symbolic name that is a reference or pointer to an object. Once an object is assigned to a variable, you can refer to the object by that name. But the data itself is still contained within the object."
+    },
+    {
+        "topic": "Floor division",
+        "definition": "In Python, we can perform floor division (also sometimes known as integer division) using the // operator."
+    },
+    {
+        "topic": "API",
+        "definition": "API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you’re using an API.."
+    },
+    {
+        "topic": "Calling a Function",
+        "definition": "Running the function by putting () after its name. example: print()"
+    }
+]
