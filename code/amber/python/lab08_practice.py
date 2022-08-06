79ff00041d0fb59a095412eeebd1b37e3cf18e9d
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-05 20:19:12 -0700

    Add num matches function with while to iterate through loops and for to compare ticket numbers.

--- code/amber/python/lab08_practice.py
+++ code/amber/python/lab08_practice.py
@@ -13,6 +13,37 @@ def pick_6():
 
 winning_ticket = pick_6()
 
-balance = 0.00
+#balance = 0.00
 
 picked_ticket = []
+
+
+def num_matches(winning, ticket):
+
+  balance = 0.00
+  counter = 0
+
+  while counter < 10: # make this 100000 later
+    ticket = pick_6()
+    counter += 1
+    balance -= 2.00
+    match = 0
+
+    for i in range(0, len(ticket)):
+      if winning[i] == ticket[i]:
+        print(f'{winning[i]} and {ticket[i]} match!')
+        match += 1
+
+
+
+
+
+
+
+
+
+
+
+# num_matches(winning, ticket) which returns the number of matches between the winning numbers and the ticket.
+
+# Find how many numbers match
