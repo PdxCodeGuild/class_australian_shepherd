edb00631e95415b5ba894571b84c3b396866838e
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-09 21:34:26 -0700

    Add variables to count each sock, a loop to sort them from random list, and variables to calculate pairs and loose socks for each type.

--- code/amber/python/optional_socks.py
+++ code/amber/python/optional_socks.py
@@ -8,3 +8,27 @@ rando_socks = []
 
 while len(rando_socks) < 100:
   rando_socks.append(random.choice(sock_types))
+
+ankle = 0
+crew = 0
+calf = 0
+thigh = 0
+
+for each in rando_socks:
+  if each == 'ankle':
+    ankle += 1
+  elif each == 'crew':
+    crew += 1
+  elif each == 'calf':
+    calf += 1
+  elif each == 'thigh':
+    thigh += 1
+
+ankle_pair = ankle // 2
+ankle_loose = ankle % 2
+crew_pair = crew // 2
+crew_loose = crew % 2
+calf_pair = calf // 2
+calf_loose = calf % 2
+thigh_pair = thigh // 2
+thigh_loose = thigh % 2
