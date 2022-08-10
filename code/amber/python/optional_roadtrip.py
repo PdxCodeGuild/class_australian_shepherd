285056e2bbfdecbab7511234e99f155380200fc9
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-09 19:24:41 -0700

    Add for loop that checks for city input connections and prints them and their other connections to complete version 1.

--- code/amber/python/optional_roadtrip.py
+++ code/amber/python/optional_roadtrip.py
@@ -14,3 +14,12 @@ city_input = input('Please enter a city from those listed above: ').title()
 
 if city_input in city_to_accessible_cities.keys():
   print(f'The following is a list of cities directly connected to {city_input}: {city_to_accessible_cities[city_input]}.') #maybe fix this so it is not printed as dict
+
+print(f'From the connections listed, the following additional connections can be made: ')
+
+connects = city_to_accessible_cities.pop(city_input)
+
+for connect in connects:
+  if connect in city_to_accessible_cities.keys():
+    hop_hop = list(city_to_accessible_cities.pop(connect))
+    print(f'{connect}: {", ".join(hop_hop)}')
