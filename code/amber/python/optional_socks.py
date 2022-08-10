e47a7a93450e56aefc1c8589d247bb5f4850637d
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-09 21:31:47 -0700

    Add variables for sock types and random socks generated by while loop.

--- code/amber/python/optional_socks.py
+++ code/amber/python/optional_socks.py
@@ -1,1 +1,10 @@
 # SOCK SORTER
+
+import random
+
+sock_types = ['ankle', 'crew', 'calf', 'thigh']
+
+rando_socks = []
+
+while len(rando_socks) < 100:
+  rando_socks.append(random.choice(sock_types))
