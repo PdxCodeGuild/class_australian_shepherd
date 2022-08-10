903bb7dcd4c09c44c7f6fa440212deca3d9ae2e2
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-09 20:04:53 -0700

    Add line formatting/spacing to print statements.

--- code/amber/python/optional_roadtrip.py
+++ code/amber/python/optional_roadtrip.py
@@ -8,18 +8,27 @@ city_to_accessible_cities = {
   'Philadelphia': {'New York'}
 }
 
-print(list(city_to_accessible_cities.keys()))
+cities_list = list(city_to_accessible_cities.keys())
 
-city_input = input('Please enter a city from those listed above: ').title()
+print(", ".join(cities_list))
+
+city_input = input('''
+Please enter a city from those listed above: ''').title()
+
+hop = list(city_to_accessible_cities[city_input])
 
 if city_input in city_to_accessible_cities.keys():
-  print(f'The following is a list of cities directly connected to {city_input}: {city_to_accessible_cities[city_input]}.') #maybe fix this so it is not printed as dict
+  print(f'''\nCities that directly connect to {city_input}:
 
-print(f'From the connections listed, the following additional connections can be made: ')
+{", ".join(hop)}.
+
+  ''')
+
+print(f'''From the connections listed, the following connections are possible: ''')
 
 connects = city_to_accessible_cities.pop(city_input)
 
 for connect in connects:
   if connect in city_to_accessible_cities.keys():
     hop_hop = list(city_to_accessible_cities.pop(connect))
-    print(f'{connect}: {", ".join(hop_hop)}')
+    print(f'\n{connect}: {", ".join(hop_hop)}.\n')
