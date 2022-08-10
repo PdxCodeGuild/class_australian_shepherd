114079c7348912fd0cc8851cb35ea6ff84aace18
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-09 19:08:51 -0700

    Add city to accessible cities dictionary and print statement that prints its keys.

---
+++ code/amber/python/optional_roadtrip.py
@@ -0,0 +1,11 @@
+# ROAD TRIP
+
+city_to_accessible_cities = {
+  'Boston': {'New York', 'Albany', 'Portland'},
+  'New York': {'Boston', 'Albany', 'Philadelphia'},
+  'Albany': {'Boston', 'New York', 'Portland'},
+  'Portland': {'Boston', 'Albany'},
+  'Philadelphia': {'New York'}
+}
+
+print(list(city_to_accessible_cities.keys()))
