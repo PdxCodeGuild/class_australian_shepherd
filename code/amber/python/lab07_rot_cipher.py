45628a5910b78741403e703ce416d01339fa5f5a
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-12 12:53:15 -0700

    Replace english to rot dict with separate lists of each's values, an empty string to hold user rotated key values and for loops to rotate, completing version 2.

--- code/amber/python/lab07_rot_cipher.py
+++ code/amber/python/lab07_rot_cipher.py
@@ -1,3 +1,42 @@
+#------------------------------------------------------------------------------------------
+#Version 2
+
+#Allow the user to input the amount of rotation used in the encryption / decryption.
+
+#Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
+english =	['a', 'b', 'c', 'd', 'e',	'f',	'g',	'h',	'i',	'j',	'k',	'l',	'm',	'n', 'o',	'p', 'q',	'r', 's', 't', 'u', 'v', 'w',	'x', 'y', 'z']
+
+rot_13 =	['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',	'k', 'l',	'm']
+# starts at 13th index
+user_rotated = []
+
+#secrets = input('Please enter words for encryption: ')
+#print(secrets)
+
+rotation = input('Please also enter an integer between 1 and 26 to denote how much rotation of your cipher you would like: ')
+
+rotation = int(rotation)
+
+#rotation = english[rotation]
+print(rotation)
+
+
+for index in range(rotation, len(english)):
+  user_rotated.append(english[index])
+  #print(index, english[index])
+for index in range(0, rotation - 1):
+  user_rotated.append(english[index])
+
+print(user_rotated)
+
+#for each in secrets:
+#  if each in english_rot.keys():
+#    secrets = secrets.replace(each, english_rot[each])
+
+#print(secrets)
+
+
+'''
 # ROT CIPHER
 
 english_rot = {
@@ -30,11 +69,11 @@ english_rot = {
 }
 
 secrets = input('Please enter words for encryption: ')
-
-print(secrets)
+#print(secrets)
 
 for each in secrets:
   if each in english_rot.keys():
     secrets = secrets.replace(each, english_rot[each])
 
 print(secrets)
+'''
