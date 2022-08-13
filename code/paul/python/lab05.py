7a230860e2050e58cf9b77c6e510c25871bab832
PaaulRagland1992 <pjragland32@gmail.com>
2022-08-12 20:24:39 -0700

    Added anagram function per Mr. T

--- code/paul/python/lab05.py
+++ code/paul/python/lab05.py
@@ -1,6 +1,8 @@
-
+from collections import Counter
 
 word= input("Enter A Word:")
+word2= input("Enter another word:")
+
 
 def check_palindrome(word):
     reversed_word= word[::-1]
@@ -10,13 +12,19 @@ def check_palindrome(word):
     else:
         return False
 
-
-
 results= check_palindrome(word)
 
 if results == True:
     print ("This is a palindrome")
 else:
     print("not a palindrome")
+check_palindrome(word)
+
+def check_anagram(word, word2):
+    if(Counter(word) == Counter(word2)):
+        print("These are anagrams")
+    else:
+        print("Nah Fam, these aint it")
+check_anagram(word, word2)
 
     #nothing follows
