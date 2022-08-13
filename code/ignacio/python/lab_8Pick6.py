852005f16eb43e5ecb561ca33ffd01e296585919
bean daddy <ignacioramirez@bean-daddy-2.local>
2022-08-12 20:00:38 -0700

    still needs alot of work

---
+++ code/ignacio/python/lab_8Pick6.py
@@ -0,0 +1,38 @@
+# Generate a list of 6 random numbers representing the winning ticket
+
+import random
+randint = random.randint(0, 99)
+
+winning_ticket = []
+for item in range(6):
+    winning_ticket.append(randint)
+print(winning_ticket)
+# ----------------------------------------------
+# functions
+
+# def pick6():
+# # used this for winning ticket and my ticket
+
+# def num_matches(winning, ticket):
+
+# # ----------------------------------------------
+# # Start your balance at 0
+# winnings = 0
+
+# # Loop 100,000 times, for each loop:
+
+
+# # Generate a list of 6 random numbers representing the ticket
+# my_ticket = []
+
+# # Subtract 2 from your balance (you bought a ticket)
+# winnings -= winnings
+
+# # Find how many numbers match
+# winning_ticket == my_ticket
+
+# # Add to your balance the winnings from your matches
+
+
+# # After the loop, print the final balance (Hint: This will be negative)
+
