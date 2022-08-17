bdcb479b76c2eb48ee56e3b6132a88b1274adf3b
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-16 21:37:28 -0700

    Add create and write functions for 'C' part of CRUD for version 2.

--- code/amber/python/lab11_contact_list.py
+++ code/amber/python/lab11_contact_list.py
@@ -29,6 +29,38 @@ for index in range(1, len(lines)):
 
   contact_list.append(contact)
 
+  # V. 2 -------------------------
+
+
+#Implement a CRUD REPL
+
+#Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
+
+def create():
+  name = input('Name: ').title()
+  fruit = input('Favorite Fruit: ').title()
+  color = input('Favorite Color: ').title()
+
+  person = {
+    'Name': name,
+    'Favorite Fruit': fruit,
+    'Favorite Color': color
+  }
+
+  write(person)
+
+def write(person):
+  contact_list.append(person)
+
+create()
+
+#print(f'contact list with "C": {contact_list}')
+
+#Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
+
+#Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
+
+#Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list.
 
 
 
