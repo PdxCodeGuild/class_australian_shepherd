ce9ac5a7a56931b369650dc888bf99064236b848
bbirky <bbirky907@gmail.com>
2022-08-17 20:53:15 -0700

    ben_b-python-lab11_ContactList-version2

--- code/ben_b/python/lab11_ContactList.py
+++ code/ben_b/python/lab11_ContactList.py
@@ -1,20 +1,112 @@
+'''
+benjamin_birky
+lab11_ContactList
+08/17/2022
+'''
+#---------------------------------------------Version 2 --------------------------------------------
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
 
-contacts_list = []
-contact_dict = {}
-
+contacts = []
+new_contacts = []
 header_list = lines[0].split(',')
 
-for line in range(1, len(lines)):
-    temp_line = lines[line].split(',')
-    contact = dict(zip(header_list, temp_line))
-    contacts_list.append(contact)
+# Import CSV contacts
+def importContacts():
+    for line in range(1, len(lines)):
+        temp_line = lines[line].split(',')
+        csv_contact = dict(zip(header_list, temp_line))
+        addContact(csv_contact)
+    return
    
+# Add contact
+def addContact(person):
+    contacts.append(person)
 
-print(contacts_list)
+# Create a contact, add the person to contact list
+def createContact():
+    name = input("Name: ").lower()
+    age = input("Age: ")
+    city = input("City: ").lower()
+
+    person = {
+            'name' : name,
+            'age' : age,
+            'city' : city
+    }
+    addContact(person)
+    return
+
+# Retrieve a contact
+def retrieveContact():
+    name = input("What is the contact's name?: ")
+    for person in contacts:
+        if name == person['name']:
+            print(f"Name: {person['name']}, Age: {person['age']}, City: {person['city']}")
+            break
+
+# Update a contact
+def updateContact():
+    name = input("What is the contact's name?: ").lower()
+    attribute = input("What attribute do you want to change?: ").lower()
+    value = input("What is the new value for the attribute?: ")
+    for person in contacts:
+        if name == person['name']:
+            person.update({attribute: value})
+            contacts.append(person)
+            break
+
+# Delete a contact
+def deleteContact():
+    name = input("What is the contact's name?: ")
+    for person in contacts:
+        if name == person['name']:
+            del person
+            break
+
+# Print contacts
+def printContacts():
+    for person in contacts:
+        print(f"Name: {person['name']}\tAge: {person['age']} \tCity: {person['city']}")
 
 
+# CRUD REPL
+importContacts()
+
+while True:
+    option = input('''
+1) Create a contact
+2) Retrieve a contact
+3) Update a contact
+4) Delete a contact
+Select a number or type any key to exit
+What would you like to do?: ''')
+    if option == '1':
+        createContact()
+    elif option == '2':
+        retrieveContact()
+    elif option == '3':
+        updateContact()
+    elif option == '4':
+        deleteContact()
+    else:
+        break
+         
+printContacts()
+
+#---------------------------------------------Version 3 --------------------------------------------
+# Export to new_contacts.csv
+for person in range(len(contacts)):    
+    thisdict = contacts[0]
+    thisdict = thisdict.get('name')
+    print(thisdict)
+
+
+
+
+dummy_data = lines[0]
+with open('new_contacts.csv', 'w') as file:
+        file.write(dummy_data)
 
 
 
@@ -36,124 +128,7 @@ dummy_data = lines[0]
 with open('new_contacts.csv', 'w') as file:
         file.write(dummy_data)
 
-
-    contacts = [
-    {'name':'matthew', 'favorite fruit':'blackberries', 'favorite color':'orange'},
-    {'name':'sam', 'favorite fruit':'pineapple' ...}
-]
-
-# CRUD 
-
-# CRUD L 
-
-
-contacts = [
-    {
-        'name': 'sara',
-        'age': 25,
-        'city': 'portland'
-    },
-        {
-        'name': 'bob',
-        'age': 99,
-        'city': 'florida'
-    }
-]
-
-
-# Crete a single person
-def create():
-    name = input("Name: ")
-    age = input("Age: ")
-    city = input("City: ")
-
-    person = {
-        'name': name,
-        'age': age,
-        'city' : city
-    }
-
-    write(person)
-
-# Write the change
-def write(person):
-    contacts.append(person)
-
-
-
-# Retrieve a single person
-def retrieve():
-    name = input("Enter the name of the person you are searching for: ")
-    for person in contacts:
-        # print(person['city'])
-
-        if person['name'] == name:
-            print(f"""
-            Name: {person['name']}
-            Age: {person['age']}
-            City: {person['city']}
-            \n""")
-
-    return name
-
-def update():
-    name = retrieve()
-    choice = input("Update their name, age or city? ")
-    new_value = input("Change the value to: ")
-
-    for person in contacts:
-        if person['name'] == name:
-            if choice == 'name':
-                person['name'] = new_value
-            elif choice == 'age':
-                person['age'] = new_value
-            elif choice == 'city':
-                person['city'] = new_value
-
-
-def delete():
-    name = retrieve()
-
-    for index in range(len(contacts)):
-        if name == contacts[index]['name']:
-            contacts.pop(index)
-            break
-
-def list():
-    counter = 1
-    for person in contacts:
-        print(f"{counter}. {person['name']}, {person['age']} {person['city']}")
-        counter += 1
-
-
-
-def main():
-    while True:
-        choice = input(Welcome what would you like to do to your contacts
-        1: Create a new contact
-        2: Retrieve a record
-        3: Update a record
-        4: Delete a record
-        5: List all records
-        )
-
-        if choice == '1':
-            create()
-
-        elif choice == '2':
-            retrieve()
-
-        elif choice == '3':
-            update()
-
-        elif choice == '4':
-            delete()
-
-        elif choice == '5':
-            list()
-    
-    
-main()
-
-
-    '''
+Version 3
+When REPL loop finishes, write the updated contact info to the CSV file to be saved. 
+I highly recommend saving a backup contacts.csv because you likely won't write it correctly the first time.
+'''
