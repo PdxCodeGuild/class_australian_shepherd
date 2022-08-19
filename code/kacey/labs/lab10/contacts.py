fa7b053a05c30ac2703995365ffce1494a38ad69
Kacey Broetje <kaceylauinger@gmail.com>
2022-08-18 19:23:33 -0700

    finished lab10 Contacts.csv

--- code/kacey/labs/lab10/contacts.py
+++ code/kacey/labs/lab10/contacts.py
@@ -1,19 +1,16 @@
-
 #        LAB10       CONTACTS.CSV         #
 
 
-with open('contacts.csv', 'r') as file:
-    lines = file.read().split('\n')
-    print(lines)
-    
-    
+with open("contacts.csv", "r") as file:
+    lines = file.read().split("\n")
+    # print(lines)
+
+
 # headers to create are     NAME,    Favorite FRUIT,    Favorite COLOR
 
+headers_with_commas = lines[0]
+headers = headers_with_commas.split(",")
 
-
-
-
-headers = lines[0].split(",")
 contacts_csv = []
 for line in range(1, len(lines)):
     values = lines[line].split(",")
@@ -35,6 +32,7 @@ for line in range(1, len(lines)):
 #     'Favorite Color': favorite_color
 # }
 
+
 def create():
     new_user = {}
     for header in headers:
@@ -44,18 +42,18 @@ def create():
 
 def retrieve(csv_list, users_name):
     for index in range(len(csv_list)):
-        if csv_list[index]['Name'] == users_name:
-            
+        if csv_list[index]["Name"] == users_name:
+
             return csv_list[index]
 
 
 def update(contacts_csv, user_update):
-    
+
     # contacts = retrieve(contacts_csv, user_update)
     # print(contacts)
     # contact_updates = contacts
     # print(contact_updates.keys(), contact_updates.values(), ' this is the contact_updates keys & values')
-    #bring forth users information to change/update
+    # bring forth users information to change/update
     which_value = input(f"What value do you wish to update?:\n{headers} ").lower()
     update_value = input(f"What is the updated value?: ")
     for contact in contacts_csv:
@@ -67,80 +65,74 @@ def update(contacts_csv, user_update):
             if which_value == "favorite color":
                 contact["Favorite Color"] = update_value
         # print(contact)
-    
+
+
 def delete(contacts_csv, which_contact):
-    
+
     for contact in range(len(contacts_csv)):
-        # print(contact)
+
         if contacts_csv[contact]["Name"] == which_contact:
-            # contacts_csv.remove(contact[which_contact])
+
             del contacts_csv[contact]
             print(contacts_csv)
             break
     print(contacts_csv)
-    
-def save(contacts_csv):
-    
-    contacts_csv = str(contacts_csv)
-    
-    with open('new_contacts.csv', 'w') as file:
-        file.write(contacts_csv)
-        
-#     headers = lines[0].split(",")
-# contacts_csv = []
-# for line in range(1, len(lines)):
-#     values = lines[line].split(",")
-#     contact = {}
-#     for i in range(len(headers)):
-#         contact[headers[i]] = values[i]
-#     contacts_csv.append(contact)
+
+
+def save(contacts_csv, headers_string, headers_list):
+    with open("contacts.csv", "w") as file:
+        working_string_csv = ""
+        working_string_csv += headers_string + "\n"
+
+        for contact in contacts_csv:
+
+            string_line = ""
+# try using an f string to add the contact
+            string_line += contact[headers_list[0]]
+            string_line += "," + contact[headers_list[1]]
+            string_line += "," + contact[headers_list[2]]
+
+            working_string_csv += string_line + "\n"
+        working_string_csv = working_string_csv[:-1]
+
+        file.write(working_string_csv)
 
 
 while True:
-    user_input = input("""Type the number of the command you want: 
+    user_input = input(
+        """Type the number of the command you want: 
         1 = Create
         2 = Retrieve
         3 = Update
         4 = Delete
         5 = Save
         6 = "Exit"\n
-        """)
-    
+        """
+    )
+
     if user_input == "1":
         contacts_csv.append(create())
         # print(contacts_csv)
-        
+
     elif user_input == "2":
         users_name = input("Please enter a users name to display their information: ")
         contact = retrieve(contacts_csv, users_name)
         for value in contact:
             print(f"{value}: {contact[value]}\n")
-        
+
     elif user_input == "3":
-        user_update = input("Please enter the name of the user you would like to update their information: ")
+        user_update = input(
+            "Please enter the name of the user you would like to update their information: "
+        )
         update(contacts_csv, user_update)
         pass
-    
+
     elif user_input == "4":
         which_contact = input(f"Which contact would you like to delete?: ")
         delete(contacts_csv, which_contact)
-        
+
     elif user_input == "5":
-        save(contacts_csv)
-        
+        save(contacts_csv, headers_with_commas, headers)
+
     else:
         break
-    
-    
-    
-    # how to add lines to the csv
-    
-# person = {
-#         'name': "matt",
-#         'fruit': 'apple',
-#         'color': 'green'
-#     }
-    
-# dummy_data = f"name,fruit,color\n{person['name']},{person['fruit']},{person['color']}"
-    
-
