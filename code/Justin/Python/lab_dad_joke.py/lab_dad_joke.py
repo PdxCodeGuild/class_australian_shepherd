c9720cdb0a956efb70955f20936b5cfa404d5a1b
jmgraham5 <83609009+jmgraham5@users.noreply.github.com>
2022-08-23 00:40:48 -0400

    updated lab_dad_joke

--- code/Justin/Python/lab_dad_joke.py/lab_dad_joke.py
+++ code/Justin/Python/lab_dad_joke.py/lab_dad_joke.py
@@ -1,13 +1,29 @@
-# Importing requests and JSON
+# # Importing requests and JSON
+# import requests
+# import json
+# # Setting our headers to accept application/json
+# headers = {'Accept': 'application/json'}
+# # Settting our responses with our header for our webpage
+# response = requests.get("https://icanhazdadjoke.com", headers=headers)
+# # Using json method to load our request as text
+# data = json.loads(response.text)
+# # Setting our joke to a variable
+# joke = data['joke']
+# # Printing our joke
+# print(joke)
+
 import requests
 import json
 # Setting our headers to accept application/json
 headers = {'Accept': 'application/json'}
+parameters = {'Accept': 'term'}
 # Settting our responses with our header for our webpage
-response = requests.get("https://icanhazdadjoke.com", headers=headers)
+search_term = input('What type of joke would you like to hear? \n ==>').lower()
+response = requests.get(f"https://icanhazdadjoke.com/search?term={search_term}",
+                        headers=headers, params=parameters)
 # Using json method to load our request as text
-data = json.loads(response.text)
-# Setting our joke to a variable
-joke = data['joke']
-# Printing our joke
-print(joke)
+data = json.loads(response.text)['results']
+# Cleaning up print data
+for jokes in data:
+    j = jokes['joke']
+    print(j+'\n')
