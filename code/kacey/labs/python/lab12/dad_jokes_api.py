6d4c854cc272a1dd1a7d0086326a3959428a3d6b
Kacey Broetje <kaceylauinger@gmail.com>
2022-08-22 21:02:18 -0700

    submitting dad jokes API

---
+++ code/kacey/labs/python/lab12/dad_jokes_api.py
@@ -0,0 +1,59 @@
+import requests, json
+import time
+
+
+
+# url = "https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id" 
+
+# response = requests.get(url)
+
+# data = json.loads(response.text)
+
+# # print(data)
+
+# image_id = data['data']['image_id']
+
+# title = data['data']['title']
+
+# image_url = f'https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg'
+
+# print(f"""Title: {title}
+#     Url: {image_url}""")
+
+# print(image_id)
+
+url = "https://icanhazdadjoke.com/"
+# print(response.text)
+# print(joke)
+
+
+
+
+# answer = True
+# while answer:
+#     response = requests.get(url, headers={'Accept': 'application/json'})
+#     data = json.loads(response.text)
+#     joke = data['joke']
+#     answer = input("Wanna hear a dad joke?, Yes or No: ").lower()
+    
+    
+#     if answer == "yes":
+#         print(joke)
+#         time.sleep(3)
+#         print("hahaha")
+#         answer = True
+#     if answer == "no":
+#         print("Okay, your loss!")
+#         answer = False
+        
+
+term = input("Please enter a search term: ")
+
+url = f"https://icanhazdadjoke.com/search?term={term}"
+# print(url)
+response = requests.get(url, headers={'Accept': 'application/json'})
+
+data = json.loads(response.text)
+for i in range(len(data)):
+    print(data['results'][i]['joke'], "\n")
+
