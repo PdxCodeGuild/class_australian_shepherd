66235278fc054d9fe56b34dfb2d4ba7da2c02f99
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-24 15:57:10 -0700

    Add import request and json modules, qotd url variable, qotd response variable to send get request, and qotd data variable to contain response text.

--- code/amber/python/lab15_quotes_api.py
+++ code/amber/python/lab15_quotes_api.py
@@ -1,1 +1,10 @@
 # QUOTES API
+
+import requests, json
+
+qotd_url = f'https://favqs.com/api/qotd'
+
+qotd_response = requests.get(
+    qotd_url, headers={'Accept': 'application/json'})
+
+qotd_data = json.loads(qotd_response.text)
