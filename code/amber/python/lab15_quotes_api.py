ca18845413442acdbdc60b0336313d32f2ee4c14
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-24 16:02:01 -0700

    Add url variable, response variable with authorization token, and data variable to contain response text for version 2.

--- code/amber/python/lab15_quotes_api.py
+++ code/amber/python/lab15_quotes_api.py
@@ -8,9 +8,20 @@ qotd_response = requests.get(
     qotd_url, headers={'Accept': 'application/json'})
 
 qotd_data = json.loads(qotd_response.text)
+#print(qotd_data)
 
 qotd = qotd_data['quote']['body']
 qotd_author = qotd_data['quote']['author']
 
-print(qotd)
-print(qotd_author)
+#print(qotd)
+#print(qotd_author)
+
+# v.2 - list quotes by keyword
+
+url = f'https://favqs.com/api/quotes'
+
+response = requests.get(
+    url, headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+
+data = json.loads(response.text)
+#print(data)
