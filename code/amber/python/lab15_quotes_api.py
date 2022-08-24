cc8069843cd56c2be863ed7f960cba0b47c20000
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-24 15:59:13 -0700

    Add qotd and qotd author variables and print statements to return data values to user.

--- code/amber/python/lab15_quotes_api.py
+++ code/amber/python/lab15_quotes_api.py
@@ -8,3 +8,9 @@ qotd_response = requests.get(
     qotd_url, headers={'Accept': 'application/json'})
 
 qotd_data = json.loads(qotd_response.text)
+
+qotd = qotd_data['quote']['body']
+qotd_author = qotd_data['quote']['author']
+
+print(qotd)
+print(qotd_author)
