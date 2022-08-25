cbbcf67abcf6a540c8290f62ef4f00551322d503
PaaulRagland1992 <pjragland32@gmail.com>
2022-08-24 20:39:47 -0700

    Should not have bug now. Ref 102 lab. See comments

--- code/paul/python/lab05.py
+++ code/paul/python/lab05.py
@@ -1,36 +1,36 @@
 
 
-word= input("Enter A Word:")
+
+
+
+
+
+
+
+word1= input("Enter A Word:")
 word2= input("Enter another word:")
 
 
-def check_palindrome(word):
-    reversed_word= word[::-1]
-    
-    if word == reversed_word:
-        return True
+def check_palindrome(word): #[::-1] <---- This is for string slicing. These are lists below. Do not use.
+    word = list(word)
+    rev_word = list(reversed(word)) #<--- this reverse method creates a new list instead of changing the original list
+    print(word)
+    print(rev_word)
+    if word == rev_word:
+        print("This is a palindrome")
     else:
-        return False
-
-results= check_palindrome(word)
-
-if results == True:
-    print ("This is a palindrome")
-else:
-    print("not a palindrome")
-check_palindrome(word)
-
-def check_anagram(words):
-    word_ = word[::1]
+        print("Nah. Not a Palindrome")
+# check_palindrome(word1)
     
-    if word == word_:
-        return True
-    else:
-        return False
-results = check_anagram(word)
 
-if results == True:
-    print ("This is an anagram")
-else:
-    print("not a anagram")
-check_anagram(word)
+def check_anagram():  #<---- Anagram lab from 102 reference. 
+    list_1= list(word1)
+    list_2= list(word2)
+    list_1.sort()
+    list_2.sort()
+    if list_1==list_2:
+        print("These are anagrams")
+    
+    else:
+        print("These are not anagrams")
+check_anagram()
