a0929e5a078d816144e94af00a6be87d48a2ffda
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-26 21:28:14 -0700

    Add console function and use on opening print statement and play input.

--- code/amber/python/mini_cap.py
+++ code/amber/python/mini_cap.py
@@ -14,3 +14,13 @@ table.add_column("Title", justify="center", style="yellow")
 table.add_column("URL",  justify="center", style="cyan")
 table.add_column("Liked",  justify="center", style="green")
 table.add_column("Disliked",  justify="center", style="magenta")
+
+console = Console()
+
+console.print('''\n[bold]F*CK ____ ... LET'S ART![/bold]\n
+\nThis exercise allows the user to view pieces of art selected at random from the Met Museum archives.
+\nAfter each piece is generated, you must click on the URL provided in order to view it. Afterward, you can return to the terminal and submit information regarding your enjoyment of the piece before continuing on.
+\nWhen you are done playing, a report will be returned to you with the information associated with each piece you viewed.
+\n[dim yellow]The goal of this exercise is to provide an opportunity for spontanaiety, self-reflection, and enjoyment through the appreciation of art.[/dim yellow]\n''', justify="center", style="yellow")
+
+play = console.input("[underline italic]Would you like to check out some random art?[/underline italic] [bold green]Y[/bold green]/[bold red]N[/bold red]: ")
