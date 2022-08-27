8e4f76f68e23e2e9a157a9487939039d8d40b964
Amber-DiNenna <amberdinenna@gmail.com>
2022-08-26 21:26:43 -0700

    Add open call to art.json to reference valid object ids and rich table to display report values to user.

--- code/amber/python/mini_cap.py
+++ code/amber/python/mini_cap.py
@@ -7,3 +7,10 @@ import webview
 
 with open('art.json') as f:
   object_ids = json.load(f)
+
+table = Table(title="Thank you for playing. Here is a report of your results:")
+table.add_column("Rating", justify="center", style="bold green")
+table.add_column("Title", justify="center", style="yellow")
+table.add_column("URL",  justify="center", style="cyan")
+table.add_column("Liked",  justify="center", style="green")
+table.add_column("Disliked",  justify="center", style="magenta")
