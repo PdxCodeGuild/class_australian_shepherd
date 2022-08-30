73e2372014dd3f43a10159fe5716507764a892a2
jmgraham5 <83609009+jmgraham5@users.noreply.github.com>
2022-08-30 00:35:55 -0400

    updated python capstone

--- code/Justin/Python/mini_cap/mini_cap.py
+++ code/Justin/Python/mini_cap/mini_cap.py
@@ -8,8 +8,8 @@ from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-
-headers = {}
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 def search_cards():
@@ -34,15 +34,18 @@ print(f"You have selected {a_1['name']} from the set {a_1['set']}.")
 response = requests.get(
     f"https://api.scryfall.com/cards/multiverse/{a_1['multiverse']}")
 data = json.loads(response.text)['image_uris']
-# img = Image.open(requests.get(
-#     data['normal'], stream=True).raw)
-# img.show()
-# print(data['normal'])
+data_2 = json.loads(response.text)['purchase_uris']
+data_3 = json.loads(response.text)['prices']
+
 
 kv = """
 
+WindowManager:
+    ImageWindow:
+    InfoWindow:
 
-<ReadWriteScreen>:
+<ImageWindow@Screen>:
+    name: 'ImageWindow'
     BoxLayout:
         orientation: 'vertical'
         Label:
@@ -53,24 +56,61 @@ kv = """
         AsyncImage:
             source: root.source
         Button:
-            text: 'Exit Application'
+            text: 'See Information'
+            size_hint_y: None
+            height: 48
+            on_release: app.root.current = 'InfoWindow'
+<InfoWindow@Screen>:
+    name: 'InfoWindow'
+    BoxLayout:
+        orientation: 'vertical'
+        Label:
+            text: root.card_price
+            size_hint_y: 1
+            size_hint_x: 1
+            height: 48
+            font_size: 24
+        Button:
+            text: "Click here to buy!"
+            size_hint_y: .75
+            size_hint_x: 1
+            heigh: 10
+            font_size: 24
+            on_release: 
+                import webbrowser
+                webbrowser.open(root.card_link)
+        Button:
+            text: 'Exit'
             size_hint_y: None
             height: 48
             on_press: app.stop()
-ScreenManager:
-    ReadWriteScreen:
-        name: 'card'            
+
+
+
 
 """
 
 
-class ReadWriteScreen(Screen):
+class ImageWindow(Screen):
     card_title = StringProperty()
     source = StringProperty()
 
     def on_pre_enter(self, *args):
         self.card_title = a_1['name']
         self.source = data['normal']
+
+
+class InfoWindow(Screen):
+    card_price = StringProperty()
+    card_link = StringProperty()
+
+    def on_pre_enter(self, *args):
+        self.card_price = f" The current USD price is {data_3['usd']}."
+        self.card_link = data_2['tcgplayer']
+
+
+class WindowManager(ScreenManager):
+    pass
 
 
 class MTG_InformerApp(App):
