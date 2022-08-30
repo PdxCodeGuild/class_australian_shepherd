7bcc7fb07c2c081a0e39c9ac7aef3b2c24c9c670
Kacey Broetje <kaceylauinger@gmail.com>
2022-08-29 21:32:22 -0700

    current progress commit

--- code/kacey/labs/mini_capstone/mini_capstone.py
+++ code/kacey/labs/mini_capstone/mini_capstone.py
@@ -32,9 +32,9 @@ Questions needed answered before I begin writing code:
 2 - 
 """
 import requests
-import html
 from html import unescape
 from random import shuffle, choice
+from colorama import Fore, Back, Style
 
 #      SESSION TOKEN URL      #
 # session_token = "https://opentdb.com/api.php?amount=10&token=YOURTOKENHERE"
@@ -51,74 +51,66 @@ from random import shuffle, choice
 #     GAME TYPE QUESTIONS PROMTS     #
 num_questions = input(f'Please enter a number of questions to be asked!: 1-10: ') or '10'
 category_choice = input(f'Please enter the category number for type of questions would like to play from!\n"9" = General Knowledge\n"10" = Entertainment: Books\n"11" = Entertainment: Film \n"12" = Entertainment: Music\n"13" = Entertainment: Musicals and Theatres\n"14" = Entertainment: Television\n"15" = Entertainment: Video Games\n"16" = Entertainment: Board Games\n"17" = Science and Nature\n"18" = Science: Computers\n"19" = Science: Mathematics\n"20" = Mythology\n"21" = Sports\n"22" = Geography\n"23" = History\n"24" = Politics\n"25" = Art\n"26" = Celebrities\n"27" = Animals\nEnter Number for chosen category:') or '9'
-difficulty = input(f"Please enter the 'Questions' level of difficulty you will play at: 'easy','medium','hard': ") or 'easy'
+difficulty = input(f"Please enter the 'Questions' level of difficulty you will play at: {Fore.GREEN}'easy'{Style.RESET_ALL},{Fore.YELLOW}'medium'{Style.RESET_ALL},{Fore.RED}'hard'{Style.RESET_ALL}: {Style.RESET_ALL}") or 'easy'
+match difficulty:
+    case "easy" | "e":
+        difficulty = "easy"
+    case "medium" | "m":
+        difficulty = "medium"
+    case "hard" | "h":
+        difficulty = "hard"
+    case _:
+        difficulty = "easy"
+        
 game_type = input(f'Please enter the type of "Questions" you will be asked: "multiple" or "boolean" = (True/False): ') or 'boolean'
-
+match game_type:
+    case "multiple" | "m":
+        game_type = "multiple"
+    
 
 #      QUESTIONS REQUEST URL WITH CHOICE INPUTS     #
 questions = requests.get(f"https://opentdb.com/api.php?amount={num_questions}&category={category_choice}&difficulty={difficulty}&type={game_type}")
-print(questions, ' questions response\n')
+# print(questions, ' questions response\n')
 
 #      QUESTIONS REASSIGNED TO READABLE JSON FORMAT      #
 questions = questions.json()
 # print(questions, ' questions json readable')
 
 questions = questions['results']
+# print(questions)
 # print(html.unescape(questions['results'][1]['question']).replace('%20', ' '))
+different_colors = [Fore.GREEN,Fore.RED,Fore.YELLOW,Fore.BLUE,Fore.MAGENTA,Fore.CYAN,Fore.BLACK,Fore.WHITE]
+score = 0
 
-for index in range(len(questions)):
-    # print(questions[index]['question'])   #    no ENCODE type, gives random ass characters 
-    print(html.unescape(questions[index]['question']))# TRUE/FALSE Question
-    print(questions[index]['correct_answer'] + "\n")     # CORRECT ANSWER
-# answer = print(html.unescape(questions['results'][1]['correct_answer']).replace('%20', ' '))
-# print(answers)
+while True:
+    for index in range(len(questions)):
 
+        game_question = unescape(questions[index]['question'])# TRUE/FALSE Question
+        correct_answer = unescape(questions[index]['correct_answer'])    # CORRECT ANSWER
+        wrong_answers = unescape(questions[index]['incorrect_answers'])
+        
+        printable_wrong_answers = ""
+        for wrong_answer in wrong_answers:
+            printable_wrong_answers += f"{choice(different_colors)}{unescape(wrong_answer)}{Style.RESET_ALL} | "
 
-# print(len(questions))
+        print(f"The question is: {game_question}\nPossible Answers are: {printable_wrong_answers}{choice(different_colors)}{correct_answer}{Style.RESET_ALL}")
+        answer = input("What is your answer?: ")
+        
+        if answer == correct_answer:
+            print("Great Job! That is correct")
+            score += 1
+            print(f"Score Total: {score}")
+        elif answer == wrong_answer:
+            print("Sorry, you answered incorrectly :(")
+            print(f"Score Total: {score}")
+    print(f"In total your score was {score}\nNot bad, not bad at all :D")        
+    break
 
-# #       CODE THAT DOESN'T WORK ANYMORE       #
-# for question in range(0,len(questions['results'])):
-#     print(question, ' questions')
-#     correct_answers = questions['results'][question]['correct_answer']
-#     print(correct_answers, ' correct answers')
-#     # incorrect_answers = questions['results'][question]['incorrect_answer']
-#     # print(incorrect_answers, ' incorrect_answers')
-#     # answers = correct_answers.append(incorrect_answers)
+# print(f"The question is: {game_question}\nPossible Answers are: {printable_wrong_answers}{correct_answer}")
+# for index in range(len(questions)):
+#     print(f"{unescape(questions[index]['question'])}\n")
     
-#     # print(answers, ' answers')
     
-#     print(unescape(questions['results'][question]['question']))
-    
-#     print(unescape(questions['results'][question]['correct_answer']).lower())
-    
-
-#         REWRITE CODE TO CREATE THE GAME SESSION        #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#     for index in range(len(questions)):
+#   ASK A QUESTION, INPUT A RESPONSE, COMPARE THE ANSWER TO THE INPUT, THEN ASK NEXT QUESTION. 
+# make a gooey, pop a screen out and play within that screen
