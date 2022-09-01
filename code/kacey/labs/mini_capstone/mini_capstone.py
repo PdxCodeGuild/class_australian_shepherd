f8093e660ef778522d18973542e630ec0913495b
Kacey Broetje <kaceylauinger@gmail.com>
2022-08-31 20:52:59 -0700

    another commit save

--- code/kacey/labs/mini_capstone/mini_capstone.py
+++ code/kacey/labs/mini_capstone/mini_capstone.py
@@ -37,6 +37,10 @@ from html import unescape
 from random import shuffle, choice
 from colorama import Fore, Back, Style
 
+from tkinter import *
+from tkinter import ttk
+
+
 #      SESSION TOKEN URL      #
 # session_token = "https://opentdb.com/api.php?amount=10&token=YOURTOKENHERE"
 # session_token = session_token.json ????????? what do i put here....maybe nothing since i don't have the actual session token yet. . . . . .
@@ -53,91 +57,97 @@ from colorama import Fore, Back, Style
 # )
 
 #     GAME TYPE QUESTIONS PROMTS     #
-category_choice = (
-    input(
-        f'Please enter the category number for type of questions would like to play from!\n"9" = General Knowledge\n"10" = Entertainment: Books\n"11" = Entertainment: Film \n"12" = Entertainment: Music\n"13" = Entertainment: Musicals and Theatres\n"14" = Entertainment: Television\n"15" = Entertainment: Video Games\n"16" = Entertainment: Board Games\n"17" = Science and Nature\n"18" = Science: Computers\n"19" = Science: Mathematics\n"20" = Mythology\n"21" = Sports\n"22" = Geography\n"23" = History\n"24" = Politics\n"25" = Art\n"26" = Celebrities\n"27" = Animals\nEnter Number for chosen category:'
+def game_play_selection():
+    global length_of_questions
+    global questions
+    global questions_count
+    category_choice = (
+        input(
+            f'Please enter the category number for type of questions would like to play from!\n"9" = General Knowledge\n"10" = Entertainment: Books\n"11" = Entertainment: Film \n"12" = Entertainment: Music\n"13" = Entertainment: Musicals and Theatres\n"14" = Entertainment: Television\n"15" = Entertainment: Video Games\n"16" = Entertainment: Board Games\n"17" = Science and Nature\n"18" = Science: Computers\n"19" = Science: Mathematics\n"20" = Mythology\n"21" = Sports\n"22" = Geography\n"23" = History\n"24" = Politics\n"25" = Art\n"26" = Celebrities\n"27" = Animals\nEnter Number for chosen category:'
+        )
+        or "9"
     )
-    or "9"
-)
-category_questions_count_lookup = requests.get(
-    f"https://opentdb.com/api_count.php?category={category_choice}"
-)
-category_questions_count_lookup = category_questions_count_lookup.json()
-print(
-    category_questions_count_lookup["category_question_count"]["total_question_count"],
-    "= Total Question Count",
-)
-print(
-    category_questions_count_lookup["category_question_count"][
-        "total_easy_question_count"
-    ],
-    "= Total Easy Question Count",
-)
-print(
-    category_questions_count_lookup["category_question_count"][
-        "total_medium_question_count"
-    ],
-    "= Total Medium Question Count",
-)
-print(
-    category_questions_count_lookup["category_question_count"][
-        "total_hard_question_count"
-    ],
-    "= Total Hard Question Count",
-)
-
-difficulty = (
-    input(
-        f"Please enter the 'Questions' level of difficulty you will play at: {Fore.GREEN}'easy'{Style.RESET_ALL},{Fore.YELLOW}'medium'{Style.RESET_ALL},{Fore.RED}'hard'{Style.RESET_ALL}: {Style.RESET_ALL}"
+    category_questions_count_lookup = requests.get(
+        f"https://opentdb.com/api_count.php?category={category_choice}"
     )
-    or "easy"
-)
-num_questions = input(f"Please enter a number of questions to be asked!: ") or "10"
-
-match difficulty:
-    case "easy" | "e":
-        difficulty = "easy"
-        questions_count = category_questions_count_lookup["category_question_count"][
+    category_questions_count_lookup = category_questions_count_lookup.json()
+    print(
+        category_questions_count_lookup["category_question_count"]["total_question_count"],
+        "= Total Question Count",
+    )
+    print(
+        category_questions_count_lookup["category_question_count"][
             "total_easy_question_count"
-        ]
-    case "medium" | "m":
-        difficulty = "medium"
-        questions_count = category_questions_count_lookup["category_question_count"][
+        ],
+        "= Total Easy Question Count",
+    )
+    print(
+        category_questions_count_lookup["category_question_count"][
             "total_medium_question_count"
-        ]
-    case "hard" | "h":
-        difficulty = "hard"
-        questions_count = category_questions_count_lookup["category_question_count"][
-            "total_hard_question_count"
-        ]
-    case _:
-        difficulty = "easy"
-        questions_count = category_questions_count_lookup["category_question_count"][
-            "total_easy_question_count"
-        ]
-
-game_type = (
-    input(
-        f'Please enter the type of "Questions" you will be asked: "multiple" or "boolean" = (True/False): '
+        ],
+        "= Total Medium Question Count",
     )
-    or "boolean"
-)
-match game_type:
-    case "multiple" | "m":
-        game_type = "multiple"
+    print(
+        category_questions_count_lookup["category_question_count"][
+            "total_hard_question_count"
+        ],
+        "= Total Hard Question Count",
+    )
+
+    difficulty = (
+        input(
+            f"Please enter the 'Questions' level of difficulty you will play at: {Fore.GREEN}'easy'{Style.RESET_ALL},{Fore.YELLOW}'medium'{Style.RESET_ALL},{Fore.RED}'hard'{Style.RESET_ALL}: {Style.RESET_ALL}"
+        )
+        or "any"
+    )
+    num_questions = input(f"Please enter a number of questions to be asked!: ") or "10"
+
+    match difficulty:
+        case "easy" | "e":
+            difficulty = "easy"
+            questions_count = category_questions_count_lookup["category_question_count"][
+                "total_easy_question_count"
+            ]
+        case "medium" | "m":
+            difficulty = "medium"
+            questions_count = category_questions_count_lookup["category_question_count"][
+                "total_medium_question_count"
+            ]
+        case "hard" | "h":
+            difficulty = "hard"
+            questions_count = category_questions_count_lookup["category_question_count"][
+                "total_hard_question_count"
+            ]
+        case _:
+            difficulty = "easy"
+            questions_count = category_questions_count_lookup["category_question_count"][
+                "total_easy_question_count"
+            ]
+
+    game_type = (
+        input(
+            f'Please enter the type of "Questions" you will be asked: "multiple" or "boolean" = (True/False): '
+        )
+        or "boolean"
+    )
+    match game_type:
+        case "multiple" | "m":
+            game_type = "multiple"
 
 
-#      QUESTIONS REQUEST URL WITH CHOICE INPUTS     #
-questions = requests.get(
-    f"https://opentdb.com/api.php?amount={questions_count}&category={category_choice}&difficulty={difficulty}&type={game_type}"
-)
-# print(questions, ' questions response\n')
+    #      QUESTIONS REQUEST URL WITH CHOICE INPUTS     #
+    questions = requests.get(
+        f"https://opentdb.com/api.php?amount={num_questions}&category={category_choice}&difficulty={difficulty}&type={game_type}"
+    )
+    print(f"https://opentdb.com/api.php?amount={num_questions}&category={category_choice}&difficulty={difficulty}&type={game_type}")
+    length_of_questions = (len(questions.text[1]))
+    # print(questions, ' questions response\n')
 
-#      QUESTIONS REASSIGNED TO READABLE JSON FORMAT      #
-questions = questions.json()
-# print(questions, ' questions json readable')
+    #      QUESTIONS REASSIGNED TO READABLE JSON FORMAT      #
+    questions = questions.json()
+    # print(questions, ' questions json readable')
 
-questions = questions["results"]
+    questions = questions["results"]
 # print(questions)
 # print(html.unescape(questions['results'][1]['question']).replace('%20', ' '))
 different_colors = [
@@ -150,16 +160,19 @@ different_colors = [
     Fore.BLACK,
     Fore.WHITE,
 ]
+game_play_selection()
 score = 0
 replay = 0
 while True:
-    for index in range(int(num_questions)):
-
-        game_question = unescape(questions[index]["question"])  # TRUE/FALSE Question
-        correct_answer = unescape(questions[index]["correct_answer"])  # CORRECT ANSWER
+    for index in range(length_of_questions):
+        
+            
+        game_question = unescape(questions[index]["question"])
+        correct_answer = unescape(questions[index]["correct_answer"])
         wrong_answers = unescape(questions[index]["incorrect_answers"])
         answers = [correct_answer] + wrong_answers
-        index = index + (replay * int(num_questions))
+        shuffle(answers)
+        print(type(answers))
 
         printable_answers = ""
         for answer in answers:
@@ -171,7 +184,15 @@ while True:
             f"\nThe question is: {game_question}\nPossible Answers are: {printable_answers}"
         )
         answer = input("What is your answer?: ")
+        root = Tk()
+        frm = ttk.Frame(root, padding=10)
+        frm.grid()
+        ttk.Label(frm, text=f"\nThe question is: {game_question}\nPossible Answers are: {printable_answers}").grid(column=0, row=0)
+        # i = 0
+        for i, answer in enumerate(answers):
+            ttk.Button(frm, text=f"{answer}", command=root.destroy).grid(column=1, row=i)
 
+        root.mainloop()
         if answer == correct_answer:
             print("Great Job! That is correct")
             score += 1
@@ -186,6 +207,9 @@ while True:
     playagain = input("\nWould you like to play again? Yes or No: ").lower()
     if playagain == "yes":
         replay += 1
+        index = index + (replay * int(length_of_questions))
+        if index >= length_of_questions:
+            game_play_selection()
         print("Okay! Let's play again!\n")
 
     else:
@@ -193,8 +217,5 @@ while True:
         break
 
 
-# make a gooey, pop a screen out and play within that screen
-# on the true/false section of my code,there is a bug of sorts that needs to be resolved tomorrow. says 
-"""Traceback (most recent call last):
-File "C:\Users\kacey\github\pdx_code\class_australian_shepherd\code\kacey\labs\mini_capstone\mini_capstone.py", line 158, in <module>
-game_question = unescape(questions[index]["question"])  # TRUE/FALSE Question"""
+# make a gooey, pop a screen out and play within that scree
+
