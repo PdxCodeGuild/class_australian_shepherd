f96dd556a5c3091ce0aac8b7f08158d136bd8c5d
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-09-01 18:02:36 -0700

    reached an acceptable point, will continue to work on it

--- code/ignacio/python/pokemon-capstone.py
+++ code/ignacio/python/pokemon-capstone.py
@@ -1,58 +1,55 @@
-from tokenize import Name
-
+from calendar import prmonth
 import requests, json
 import pprint
 
 # this shall be a basic pokedex. that means this program will simplpy search and return pokemon. FOR NOW
 
-def weakness():
-    name = ''
-    url = f"https://pokeapi.co/api/v2/type/{name}"
+
+# how to return and calculate weakness mulitplier????
+
+def advantage(current_type):
+    with open('pokeweakness.json') as f:
+        types = json.load(f)
+        print(f"Advantage over:")
+        for item in types[current_type.title()].items():
+            if item[1] > 1:
+                print(f"{item[0]}")
+            # print(item[1])
+        # print(current_type)
+        # print(f"Are at a disadvantage")
+    
+
+def search_poke():
+
+    name = input('Enter a pokemon name or number from 1 to 905: ')
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
     responce = requests.get(url)
     data = json.loads(responce.text)
-    
-    print(data)
+    poke_name = data['name']
+    poke_name = poke_name.title()
+    poke_ability = data["abilities"][0]['ability']['name'].title()
+    poke_id = data['id']
+    poke_type = data['types']
+    type_list = []
+    for index in poke_type:
+        type_list.append(index['type']['name'])
+   
+    new_list = ', '.join(type_list).title()
+    # print(type_list)
+    poke_result = f"Name: {poke_name} \nNumber: {poke_id} \nType: {new_list}\nAbility: {poke_ability}"
 
-weakness()
-    
-# pokemon_list = []
-# def search_poke():
+    print(poke_result)
+    advantage(type_list[0])
 
-#     name = input('choose a mon: ')q
-#     url = f"https://pokeapi.co/api/v2/pokemon/{name}"
-#     responce = requests.get(url)
-#     data = json.loads(responce.text)
-#     poke_name = data['name']
-#     poke_name = poke_name.title()
-#     poke_ability = data["abilities"][0]['ability']['name'].title()
-#     poke_id = data['id']
-#     poke_type = data['types']
-#     type_list = []
-#     for index in poke_type:
-#         type_list.append(index['type']['name'])
-    
-#     new_list = ', '.join(type_list).title()
-#     poke_result = f"Name: {poke_name} \nNumber: {poke_id} \nType: {new_list}\nAbility: {poke_ability}"
+# pokemon = get_poke()
+while True:
+    choice = input("""\nEnter your selction
+    1: Search for pokemon
+    2: Exit
+    """)
 
-
-
-#     # poke_dict = {
-#     #     'name' : 'x',
-#     # }
-
-#     # pokemon_list.append(poke_dict)
-#     # return data 
-#     print(poke_result)
-
-
-# # pokemon = get_poke()
-# while True:
-#     choice = input("""Enter your selction
-#     1: Search for pokemon
-#     2: Done
-#     """)
-
-#     if choice == '1':
-#         search_poke()
-#     elif choice == '2':
-#         break
+    if choice == '1':
+        search_poke()
+    elif choice == '2':
+        print("Goodbye!!")
+        break
