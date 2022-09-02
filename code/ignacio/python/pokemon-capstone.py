4685ac85d07e4bb5a6393f00a4e6809ac715fdd8
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-09-01 18:04:48 -0700

    reached an acceptable point, will continue to work on it

--- code/ignacio/python/pokemon-capstone.py
+++ code/ignacio/python/pokemon-capstone.py
@@ -1,12 +1,7 @@
-from calendar import prmonth
 import requests, json
 import pprint
 
 # this shall be a basic pokedex. that means this program will simplpy search and return pokemon. FOR NOW
-
-
-# how to return and calculate weakness mulitplier????
-
 def advantage(current_type):
     with open('pokeweakness.json') as f:
         types = json.load(f)
@@ -14,14 +9,10 @@ def advantage(current_type):
         for item in types[current_type.title()].items():
             if item[1] > 1:
                 print(f"{item[0]}")
-            # print(item[1])
-        # print(current_type)
-        # print(f"Are at a disadvantage")
     
 
 def search_poke():
-
-    name = input('Enter a pokemon name or number from 1 to 905: ')
+    name = input('Enter a pokemon name or a number from 1 to 905: ')
     url = f"https://pokeapi.co/api/v2/pokemon/{name}"
     responce = requests.get(url)
     data = json.loads(responce.text)
@@ -35,15 +26,14 @@ def search_poke():
         type_list.append(index['type']['name'])
    
     new_list = ', '.join(type_list).title()
-    # print(type_list)
-    poke_result = f"Name: {poke_name} \nNumber: {poke_id} \nType: {new_list}\nAbility: {poke_ability}"
 
+    poke_result = f"Name: {poke_name} \nNumber: {poke_id} \nType: {new_list}\nAbility: {poke_ability}"
     print(poke_result)
     advantage(type_list[0])
 
-# pokemon = get_poke()
+
 while True:
-    choice = input("""\nEnter your selction
+    choice = input("""\nEnter your selction:
     1: Search for pokemon
     2: Exit
     """)
@@ -51,5 +41,5 @@ while True:
     if choice == '1':
         search_poke()
     elif choice == '2':
-        print("Goodbye!!")
+        print("Best of luck on your journey")
         break
