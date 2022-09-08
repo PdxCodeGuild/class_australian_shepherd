8e368e8703fc142b5af0f5f65b970ab9697cbabc
PaaulRagland1992 <pjragland32@gmail.com>
2022-09-07 17:13:11 -0700

    Needed to add comment to clarify on line 19

--- code/paul/python/ROT13.py
+++ code/paul/python/ROT13.py
@@ -16,12 +16,12 @@ rotation= int(input("How many rotations: "))
 rot=''
 
 for i in user_message.upper():
-    rot += alpha_char[(alpha_char.index(i) + rotation) % 26]
+    rot += alpha_char[(alpha_char.index(i) + rotation) % 26] #<------ %26 is used in case the user enters a 'Z' or 'z' in their message (user_message).
     
 print(rot.lower())
     
 #uryyb- 13 Rotation
-#ufzq 
+#ufzq 5 Rotation 
 
             
 
