1d159624a66eaa1e7cc834cd76cb6c68c3183c16
PaaulRagland1992 <pjragland32@gmail.com>
2022-09-08 18:04:58 -0700

    This will be done NLT tomorrow

--- code/paul/python/Quote_API_Lab.py
+++ code/paul/python/Quote_API_Lab.py
@@ -1,28 +1,30 @@
 import requests, json
 
 link= 'https://favqs.com/api/qotd'
-# link2= 'https://favqs.com/api/quotes?page=<page>&filter=<keyword>'
 
 response= requests.get(link)
 data = json.loads(response.text)
+# print(data['quote']['body']) #<----- Random Quote
+# print(data['quote']['author']) #<----- Author of Random Quote
+page = 1
 
-# header= Authorization: Token token="YOUR_APP_TOKEN"
+while True:
+    choice= input("Enter a keyword to get quotes or type random to get a random quote: ")
+    
 
-def random_quote():
-    for i in data:
-        # if 
-            quote= {i['body']}
-            quote= int(quote)
+    link2= 'https://favqs.com/api/quotes'
+    response2= requests.get(link2, params={'filter': choice, 'page': page}, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+    data2= json.loads(response2.text)
+    # print(data2)
 
-    print(quote)
+    
+    for item in data2['quotes']:
+        print(item['body'])
+    next_page= input("Next Page: Type next page, enter another keyword, or Type done to exit: ")
 
-
-def quote_repl():
-    choice= input('''\nEnter your choice below.
-    1: Random Quote
-    2: Keyword Search
-    ''')
-
-    if choice == '1':
-        random_quote()
-quote_repl()
+    if next_page == 'next page':
+        page += 1
+        
+    else:
+        choice == 'done'
+        break
