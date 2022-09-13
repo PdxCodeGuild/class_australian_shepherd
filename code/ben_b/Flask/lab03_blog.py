599b9bea6f347dd860b42ae4131f147a111df155
bbirky <bbirky907@gmail.com>
2022-09-12 21:40:56 -0700

    ben_b-flask-lab03_blog-WIP

---
+++ code/ben_b/Flask/lab03_blog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+import random
+
+num = random.randint(0, 100)
+print(num)
+
+colors = [
+    {
+        'name': 'red',
+        'rgb': '(255, 0, 0)'
+    },
+    {
+        'name': 'blue',
+        'rgb': '(0, 0, 255)'
+    },
+    
+
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', number=num)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', colors=colors)
+
+app.run() 
