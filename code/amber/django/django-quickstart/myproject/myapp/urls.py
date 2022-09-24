e4ea18121e2329fa63cee0c72e418c77b9970721
Amber-DiNenna <amberdinenna@gmail.com>
2022-09-23 19:09:49 -0700

    Fix timestamp issue to complete lab.

--- code/amber/django/django-quickstart/myproject/myapp/urls.py
+++ code/amber/django/django-quickstart/myproject/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('myview/', views.myview, name='myview')
+    path('', views.myview, name='myview'),
+    path('mycreate/', views.mycreate, name='mycreate')
 ]
