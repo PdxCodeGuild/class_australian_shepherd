8bfd6d240ce22202726be9f8f2575cbe220933dd
Amber-DiNenna <amberdinenna@gmail.com>
2022-09-23 21:39:45 -0700

    WIP.

--- code/amber/django/django-grocery/groceryproject/grocery_list/models.py
+++ code/amber/django/django-grocery/groceryproject/grocery_list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class GroceryModel(models.Model):
+  GroceryItem = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.GroceryItem
