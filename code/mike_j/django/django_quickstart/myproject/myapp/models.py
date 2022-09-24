973d13aa119b717bfaf0dcb7ae01406221ae420d
mjack9615 <mjack9615@gmail.com>
2022-09-23 22:06:45 -0700

    mike_j-django-quickstart

--- code/mike_j/django/django_quickstart/myproject/myapp/models.py
+++ code/mike_j/django/django_quickstart/myproject/myapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class MyModel(models.Model):
     myfield = models.CharField(max_length=200)
     mytextfield = models.TextField(blank=True, null=True)
     author = models.CharField(max_length=200, null=True)
+    timestamp = models.DateTimeField(default=timezone.now, null=True)
     
     def __str__(self):
         return self.myfield
--- code/mike_j/django/django_quickstart/myproject/myapp/views.py
+++ code/mike_j/django/django_quickstart/myproject/myapp/views.py
@@ -1,6 +1,8 @@
+from email import utils
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from .models import MyModel
+from django.utils import timezone
 
 def myview(request):
     myinstances = MyModel.objects.all()
@@ -13,6 +15,7 @@ def mycreate(request):
     myfield = request.POST['myfield']
     mytextfield = request.POST['mytextfield']
     author = request.POST['author']
-    mymodel = MyModel(myfield=myfield, mytextfield=mytextfield, author=author)
+    timestamp = timezone.now()
+    mymodel = MyModel(myfield=myfield, mytextfield=mytextfield, author=author, timestamp=timestamp)
     mymodel.save()
     return HttpResponseRedirect(reverse('myapp:myview'))
