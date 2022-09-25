458f329cacae82b920f2a272635965ed94042630
Amber-DiNenna <amberdinenna@gmail.com>
2022-09-25 14:28:25 -0700

    Render data in the template.

--- code/amber/django/django-grocery/groceryproject/grocery_list/views.py
+++ code/amber/django/django-grocery/groceryproject/grocery_list/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GroceryModel
 
 # Create your views here.
 def myview(request):
+    mygroceries = GroceryModel.objects.all()
     context = {
-        'message': 'Hello World!'
+        'mygroceries': mygroceries
     }
     return render(request, 'grocery_list/grocerytemplate.html', context)
