d7559476fc46d1334d102a49e5ba4301e5a9d55c
Amber-DiNenna <amberdinenna@gmail.com>
2022-09-28 21:12:51 -0700

    Create form, create complete view.

--- code/amber/django/django-grocery-doover/groceryproject/grocery_list/urls.py
+++ code/amber/django/django-grocery-doover/groceryproject/grocery_list/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'myapp'
+app_name = 'grocery_list'
 urlpatterns = [
-    path('', views.myview, name='myview')
+    path('', views.myview, name='myview'),
+    path('add_item/', views.add_item, name='add_item'),
+    path('complete/<int:id>/', views.complete, name='complete')
 ]
--- code/amber/django/django-grocery-doover/groceryproject/grocery_list/views.py
+++ code/amber/django/django-grocery-doover/groceryproject/grocery_list/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from .models import GroceryModel
 
@@ -10,3 +10,18 @@ def myview(request):
         'mygroceries': mygroceries
     }
     return render(request, 'grocery_list/grocerytemplate.html', context)
+
+def add_item(request):
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!', request.POST)
+    groceryitem = request.POST['groceryitem']
+    dateadded = timezone.now()
+    todo = False
+    grocerymodel = GroceryModel(groceryitem=groceryitem, dateadded=dateadded, todo=todo)
+    grocerymodel.save()
+    return redirect('grocery_list:myview')
+
+def complete(request, id):
+    groceryitem = get_object_or_404(GroceryModel, id=id)
+    groceryitem.todo = not groceryitem.todo
+    groceryitem.save()
+    return redirect('grocery_list:myview')
