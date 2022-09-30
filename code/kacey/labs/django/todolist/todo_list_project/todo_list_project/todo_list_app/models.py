8a23200a43bd94ae642246979c3a99766d09e0e4
Kacey Broetje <kaceylauinger@gmail.com>
2022-09-30 16:53:55 -0700

    progress commit, tw, extras

--- code/kacey/labs/django/todolist/todo_list_project/todo_list_project/todo_list_app/models.py
+++ code/kacey/labs/django/todolist/todo_list_project/todo_list_project/todo_list_app/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
-# Create your models here.
-
+from datetime import datetime
 
 class Todo_list(models.Model):
     title = models.CharField(max_length=100)
     objective = models.TextField(max_length=400)
     start_date = models.DateTimeField()
+    finish_date = models.DateTimeField(default=datetime.now)
     in_progress = models.BooleanField()
 
     def __str__(self):
--- code/kacey/labs/django/todolist/todo_list_project/todo_list_project/todo_list_app/views.py
+++ code/kacey/labs/django/todolist/todo_list_project/todo_list_project/todo_list_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, reverse, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
-from .models import Todo_List
+from .models import Todo_list
 
 
 def homepage(request):
-    all_list = Todo_List.objects.all()
+    all_list = Todo_list.objects.all()
     context = {
         'all_lists': all_list
     }
-    return render(request, 'todo_list_app/home.html', context)
+    return render(request, 'todo_list_app/todo.html', context)
 
 
 def new_objective(request):
@@ -18,16 +18,16 @@ def new_objective(request):
     start_date = request.POST['start_date']
     in_progress = bool(request.POST['in_progress'])
 
-    todo_list_model = Todo_List(title=title, objective=objective, start_date=start_date, in_progress=in_progress)
+    todo_list_model = Todo_list(title=title, objective=objective, start_date=start_date, in_progress=in_progress)
     todo_list_model.save()
 
     return redirect('todo_list_app:homepage')
 
 
 def edit_objective(request, id):
-    todo_list_obj = get_object_or_404(Todo_List, id=id)
+    todo_list_obj = get_object_or_404(Todo_list, id=id)
 
-    todo_list_obj.in_theatres = not todo_list_obj.in_theatres
+    todo_list_obj.in_progress = not todo_list_obj.in_progress
     todo_list_obj.save()
     return redirect('todo_list_app:homepage')
 
