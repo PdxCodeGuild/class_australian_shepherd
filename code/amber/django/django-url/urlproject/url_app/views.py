c24c47dc6ed0182fbdeae2d6a815a15791ab74ea
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-03 20:43:09 -0700

    Create test view and route.

--- code/amber/django/django-url/urlproject/url_app/views.py
+++ code/amber/django/django-url/urlproject/url_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+import random
+import string
 
 # Create your views here.
+# def random_url():
+#   length = 6
+#   url = ''.join(random.choices(string.printable, k=length))
+#   return url
+
+def home(request):
+  return HttpResponse('Make a tiny URL')
