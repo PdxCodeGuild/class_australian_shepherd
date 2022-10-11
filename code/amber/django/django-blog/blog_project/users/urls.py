ded1ea1e290bfae37e7d98605e0219b1d2c418bb
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-10 19:02:40 -0700

    Create view and route for users.

---
+++ code/amber/django/django-blog/blog_project/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'user'
+urlpatterns = [
+    path('', views.home, name='home')
+]
--- code/amber/django/django-blog/blog_project/users/views.py
+++ code/amber/django/django-blog/blog_project/users/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('hello world!')
+
+
+
+# class SignUpView(CreateView):
+#   form_class = UserCreationForm
+#   template_name = 'signup.html'
+#   success_url: reverse_lazy('login')
