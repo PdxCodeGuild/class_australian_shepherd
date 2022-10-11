1d4ee014d7f955b1edf09b4c902d13b439588fa7
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-10-11 16:34:35 -0700

    daily push

--- code/ignacio/django/Blog/myproject/my_blog/BlogApp/models.py
+++ code/ignacio/django/Blog/myproject/my_blog/BlogApp/models.py
@@ -1,3 +1,12 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class createpost(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length = 250)
+    # user
+    public = models.BooleanField(default = )
+    
+    def __str__(self):
+        return self.myfield
--- code/ignacio/django/Blog/myproject/my_blog/BlogApp/urls.py
+++ code/ignacio/django/Blog/myproject/my_blog/BlogApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'BlogApp'
 urlpatterns = [
-    path('/', views.home, name='home')
+    path('', views.home, name='home')
 ]
