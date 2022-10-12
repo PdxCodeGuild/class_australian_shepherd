2bb9312a7a63092073c05e8cde7d4187183880fd
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-10-11 17:32:23 -0700

    updating the index and views

--- code/ignacio/django/urlshortener/myproject/Shortener/ShortenerApp/urls.py
+++ code/ignacio/django/urlshortener/myproject/Shortener/ShortenerApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'ShortenerApp'
+app_name = 'shortener_app'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('transport/', views.transport, name='transport')
--- code/ignacio/django/urlshortener/myproject/Shortener/ShortenerApp/views.py
+++ code/ignacio/django/urlshortener/myproject/Shortener/ShortenerApp/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from .models import shrink
 import random, string
 
-def scrabble():
-    rando_letter = ''.join(random.choice(string.ascii_letters) for x in range(7))
-    return rando_letter
+def scrabble(request):
+    short_url = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(7))
+    return short_url
+    
 
 def homepage(request):
     objects = shrink.objects.all()
     context = {
         'objects': objects
     }
-    return render(request, 'ShortenerApp/index.html', context)
+    return render(request, 'shortener_app/index.html', context)
 
 
-def transport(request):
-        if request.method == 'POST':
-            short_url = scrabble
-            url = request.POST['url']
+def create(request):
+    url = 
+    short_url = scrabble()
+    
 
-            return HttpResponseRedirect(reverse('ShortenerApp:homepage'))
+    return HttpResponseRedirect(reverse('shortener_app:homepage'))
