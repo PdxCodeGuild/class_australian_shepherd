9b3d547513e6cfab44d4c01ec76015702a5cd29d
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-11 20:37:59 -0700

    Add edited timestamp to Post model.

--- code/amber/django/django-blog/blog_project/posts/models.py
+++ code/amber/django/django-blog/blog_project/posts/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=69)
+    body = models.TextField(max_length=666)
     author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    body = models.CharField(max_length=100)
+    edited = models.DateTimeField(null=True)
 
     def __str__(self):
         return f'{self.title} - {self.created}'
