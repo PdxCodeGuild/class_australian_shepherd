3173a7bbf2d85ea320ec53ee4c47694be9ef76f2
Kacey Broetje <kaceylauinger@gmail.com>
2022-10-11 22:32:11 -0700

    progress.need some settings. html framwork/styling

--- code/kacey/labs/django/gravy_project/gravy_app/views.py
+++ code/kacey/labs/django/gravy_project/gravy_app/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
 from django.views.generic import ListView
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
-def home(request):
-    all_gravy = Post.objects.all()
-    context = {
-        'all_gravy' :                           
-    }
-    return render(request, '')
+# def home(request):
+#     all_gravy = Post.objects.all()
+#     context = {
+#         'all_gravy' :                           
+#     }
+#     return render(request, '')
+
+################ WE USE CLASS BASED VIEWS ONLY NOW ################
+
+class ListPosts(ListView):
+    model: Post
+    template_name: 'home.html'
+
+class CreatePost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'new_post.html'
+    fields = ['title','body']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class EditPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post 
+    template_name = 'edit_post.html'
+    fields = ['title', 'body']
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('posts:home')
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.aurhor
