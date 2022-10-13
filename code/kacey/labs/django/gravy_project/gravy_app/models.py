859b958c80a98c504d099a785edea580d0ca544b
Kacey Broetje <kaceylauinger@gmail.com>
2022-10-13 16:42:16 -0700

    typo

--- code/kacey/labs/django/gravy_project/gravy_app/models.py
+++ code/kacey/labs/django/gravy_project/gravy_app/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=1000)
     user = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
-    is_public = models.BooleanField(null=True)
+    is_public = models.BooleanField(default=True)
     date_created = models.DateField(auto_now_add=True)
     date_edited = models.DateField(auto_now=True)
     
