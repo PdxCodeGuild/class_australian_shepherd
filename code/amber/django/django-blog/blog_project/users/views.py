8415286fe67446cde889188879400f7ff2489616
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-12 21:37:33 -0700

    BUGFIX: Matt's easter egg. Add styling to html.

--- code/amber/django/django-blog/blog_project/users/views.py
+++ code/amber/django/django-blog/blog_project/users/views.py
@@ -22,7 +22,7 @@ from django.shortcuts import get_object_or_404
 class SignUpView(CreateView):
   form_class = UserCreationForm
   template_name = 'signup.html'
-  success_url: reverse_lazy('login')
+  success_url = reverse_lazy('login')
 
 class ProfileView(LoginRequiredMixin, DetailView):
     model = User
