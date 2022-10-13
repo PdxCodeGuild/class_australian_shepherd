e740e53cb33b323e0860b0cc1f39fff2b260236b
Kacey Broetje <kaceylauinger@gmail.com>
2022-10-12 19:10:52 -0700

    commit to reference other branch

--- code/kacey/labs/django/gravy_project/gravy_app/models.py
+++ code/kacey/labs/django/gravy_project/gravy_app/models.py
@@ -1,4 +1,3 @@
-from turtle import title
 from django.db import models
 from django.urls import reverse
 
@@ -11,3 +10,11 @@ class BlogPost(models.Model):
     date_created = models.DateField(auto_now_add=True)
     date_edited = models.DateField(auto_now=True)
     
+    def __str__(self):
+        return f'{self.title} - {self.date_created}'
+    
+    class Meta:
+        ordering = ['-date_created']
+        
+    def get_absolute_url(self):
+        return reverse('gravy_app:home', args=(self.pk,))
