f3585f7c2f0597a8b51725455f2461763d8273dd
mjack9615 <mjack9615@gmail.com>
2022-10-20 23:24:09 -0700

    mike_j-javascript-lab01

---
+++ code/mike_j/javascript/lab01/script.js
@@ -0,0 +1,24 @@
+let nums = []
+let sum = 0
+
+let input1 = document.querySelector('#input1')
+let entBtn = document.querySelector('#entBtn')
+let disp_nums = document.querySelector('#disp_nums')
+let result = document.querySelector('#result')  
+
+entBtn.onclick = function() {
+    
+    sum += Number(input1.value)
+
+    nums.push(input1.value)
+
+    disp_nums.innerHTML = nums
+
+    let avg = (sum/nums.length)
+
+    let rounded = avg.toFixed()
+
+    result.innerHTML = rounded
+
+    console.log(sum)
+}
--- code/mike_j/python/avg_num.py
+++ code/mike_j/python/avg_num.py
@@ -1,27 +1,27 @@
 # # Version 1:
 
-# nums = [5, 0, 8, 3, 4, 1, 6]
-
-# x = 0
-
-# for num in nums:
-#     x += num
-#     print(x)
-
-# avg = x/len(nums)
-
-# print(avg)
-
-# Version 2:
-
-nums = []
+nums = [5, 0, 8, 3, 4, 1, 6]
 
 x = 0
 
-while x != "done":
-    x = input("Enter number or 'done': ")
-    if x == "done":
-        break
-    nums.append(int(x))   
+for num in nums:
+    x += num
+    print(x)
 
-print(sum(nums)/len(nums))
+avg = x/len(nums)
+
+print(avg)
+
+# Version 2:
+
+# nums = []
+
+# x = 0
+
+# while x != "done":
+#     x = input("Enter number or 'done': ")
+#     if x == "done":
+#         break
+#     nums.append(int(x))   
+
+# print(sum(nums)/len(nums))
