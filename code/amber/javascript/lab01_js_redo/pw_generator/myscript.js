9862846e3c15c9dd3b8c25d24bc9552b42ae8f78
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-20 21:40:55 -0700

    Define variables for pool, pwLength and btn. WIP: finish btn.onclick function.

--- code/amber/javascript/lab01_js_redo/pw_generator/myscript.js
+++ code/amber/javascript/lab01_js_redo/pw_generator/myscript.js
@@ -1,1 +1,15 @@
 // RANDOM PASSWORD GENERATOR
+
+let pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+pool = Array.from(pool)
+console.log(pool)
+
+
+let pwLength = document.querySelector('#pwLength')
+
+let btn = document.querySelector('#btn')
+
+btn.onclick = function() {
+  pwLength = parseInt(pwLength.value)
+  console.log(pwLength)
+}
