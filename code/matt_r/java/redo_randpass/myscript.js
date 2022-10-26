e1c8b54abae1b5d892aaad3dcb6b12549728d8ae
fmouse01 <mrodriuge75@gmail.com>
2022-10-25 21:49:28 -0700

    randpass

--- code/matt_r/java/redo_randpass/myscript.js
+++ code/matt_r/java/redo_randpass/myscript.js
@@ -3,56 +3,71 @@
 
 
 const game = ['rock', 'paper', 'scisors']
-let choice = Math.floor(Math.random() * 3);
+let choice = Math.floor(Math.random() * (2 - 0) + 0);
 
 let comp = game[choice]
+let outcome = ''
 
 console.log('comp: ' + comp)
 
-// let human = 'rock'
-let human = nextline('human')
+
+// let human = nextline('human')
 
 
-console.log('human: ' + human)
 
-if (human == 'rock') {
+
+
+
+let btn = document.querySelector('#btn')
+
+btn.onclick = function(){
+    // console.log('hello')
+
+    let humanNode = document.querySelector('#human')
+    let human = humanNode.value
+    console.log(human)
+
+    if (human == 'rock') {
     
-    if (comp == 'rock') {
-        outcome = 'its a tie'
-    } else if (comp == 'scissors') {
-        outcome = 'Player wins'
-    } else {
-        outcome = 'Computer wins'
+        if (comp == 'rock') {
+            outcome = 'its a tie'
+        } else if (comp == 'scissors') {
+            outcome = 'Player wins'
+        } else {
+            outcome = 'Computer wins'
+        }
     }
-}
-
-if (human == 'paper') {
     
-    if (comp == 'paper') {
-        outcome = 'its a tie'
+    if (human == 'paper') {
+        
+        if (comp == 'paper') {
+            outcome = 'its a tie'
+        }
+        else if (comp == 'rock') {
+            outcome = 'Player wins'
+        }
+        else {
+            outcome = 'Computer wins'
+        }
     }
-    else if (comp == 'rock') {
-        outcome = 'Player wins'
-    }
-    else {
-        outcome = 'Computer wins'
-    }
-}
-
-if (human == 'scissors'){
     
-    if (comp == 'scissors'){
-        outcome = 'its a tie'
+    if (human == 'scissors'){
+        
+        if (comp == 'scissors'){
+            outcome = 'its a tie'
+        }
+        else if (comp == 'paper'){
+            outcome = 'Player wins'
+        }
+        else {
+            outcome = 'Computer wins'
+        }
+
     }
-    else if (comp == 'paper'){
-        outcome = 'Player wins'
-    }
-    else {
-        outcome = 'Computer wins'
-    }
+    console.log(outcome)
+
+    let result = document.querySelector('#result')
+    result.innerHTML = `${outcome}, computer chose ${comp}`
 }
-
-console.log(outcome)
-
 
 
