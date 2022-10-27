a9ed88397da32a2540cb6e438921643ad2873c1d
fmouse01 <mrodriuge75@gmail.com>
2022-10-26 21:37:21 -0700

    hacked wip

---
+++ code/matt_r/java/hacker/myscript.js
@@ -0,0 +1,29 @@
+let area= document.querySelector('#area')
+
+const textArray = [
+    'hello ', 
+    "You've been hacked!",
+    '<span class="test"> andfhg a </span>',
+    '<span class="center"> hello </span>',
+]
+
+
+let content = ''
+let counter = 1
+
+document.body.addEventListener('keypress', (event) => {
+    console.log(event)
+
+    content=''
+    
+    for (let i=0; i < counter; i++){
+        
+        content += textArray[i]
+    }
+
+    counter += 1
+    
+    area.innerHTML=''
+    area.innerHTML= content
+    
+})
