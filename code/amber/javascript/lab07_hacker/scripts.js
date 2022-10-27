0032aee78b7ffdfefcfc8c3ffbf51c3cc372784f
Amber-DiNenna <amberdinenna@gmail.com>
2022-10-26 19:41:54 -0700

    Create index.html and scripts.js files with boilerplate and example code to test.

---
+++ code/amber/javascript/lab07_hacker/scripts.js
@@ -0,0 +1,13 @@
+let area = document.querySelector('#area')
+
+let content = 'username: '
+
+document.body.addEventListener('keypress', (event) => {
+    console.log(event)
+    console.log(`You pressed the key: ${event.key}`)
+
+    content += ' mr_socks '
+
+    let text = document.createTextNode(content)
+    area.appendChild(text)
+})
