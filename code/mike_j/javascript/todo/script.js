7eac83c6e36afb5de303747bd6571caab4b33300
mjack9615 <mjack9615@gmail.com>
2022-10-28 16:19:43 -0700

    mike_j-javascript-todo

--- code/mike_j/javascript/todo/script.js
+++ code/mike_j/javascript/todo/script.js
@@ -8,12 +8,21 @@ document.querySelector("#add").onclick = function() {
                 ${document.querySelector("#new").value}
             </span>
             <div class="buttons">
+                <button class="todo">ToDo</button>
                 <button class="done">Done</button>
                 <button class="delete">Delete</button>
             </div>    
         </ul>
     `
     document.getElementById("new").value = ""
+
+    let todo = document.querySelectorAll(".todo");
+    for(let i=0; i<todo.length; i++) {
+        todo[i].onclick = function() {
+            this.parentNode.previousElementSibling.style.textDecoration = "none"
+            this.parentNode.parentNode.style.color = "black"
+        }
+    }
 
     let done = document.querySelectorAll(".done");
     for(let i=0; i<done.length; i++) {
