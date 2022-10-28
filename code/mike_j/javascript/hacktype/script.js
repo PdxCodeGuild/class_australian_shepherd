e3079b5a43d07513bf2401f5f30ba80fb527ad13
mjack9615 <mjack9615@gmail.com>
2022-10-27 21:12:26 -0700

    mike_j-javascript-hacktype

---
+++ code/mike_j/javascript/hacktype/script.js
@@ -0,0 +1,13 @@
+let area = document.querySelector('#area')
+
+let content = ['inject', '<span class="error">error</span>', 'redirect', 'node', 'package', 'init', 'allocate', 'parse', 'session', 'allocate', 'packet', 'conf', '</br>', 'sudo', 'admin', '$', '#', '*', '{', '^', '@', '!', '(', '/', '?', '<', '>', '}', ')']
+
+let data = ''
+
+document.body.addEventListener('keypress', (event) => {
+    console.log(event)
+    console.log(`You pressed the key: ${event.key}`)
+
+    data += content[Math.floor(Math.random()*content.length)] + " "
+    area.innerHTML = data
+})
