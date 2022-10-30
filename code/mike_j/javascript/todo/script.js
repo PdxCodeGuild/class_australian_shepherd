caa8c7c22faef830cf54c05c167b4ff3979386ac
Tracey Stancliff <mjack9615@gmail.com>
2022-10-30 12:56:46 -0700

    mike_j-javascript-todo

--- code/mike_j/javascript/todo/script.js
+++ code/mike_j/javascript/todo/script.js
@@ -1,61 +1,38 @@
 let todoObject = [
-
     {
-
         name: 'Eat',
-
         completed: false,
-
         id: 1
-
     },
-
     {
-
         name: 'Sleep',
-
         completed: true,
-
         id: 2
-
     }   
-
 ]
 
-let submit = document.querySelector('#submit-btn')
-
-let todoSection = document.querySelector('#todos-section')
-
-let completedSection = document.querySelector('#completed-section')
+let submit = document.querySelector('#addBtn')
+let todoList = document.querySelector('#todo-list')
+let doneList = document.querySelector('#done-list')
 
 submit.onclick = function() {
-
-    console.log('you clicked the submit button, this is supposed to be for adding a new item')
-
+    
 }
 
 function addToDom(){
-
-    let todoSection = document.querySelector('#todos-section')
+    let todoList = document.querySelector('#todo-list')
 
     todoObject.forEach(task => {
-
-        todoSection.innerHTML += `<p>${task.name}</p> <button onclick="completeTodo(${task.id})">complete</button>`
-
+        todoList.innerHTML += `<p>${task.name}</p> <button onclick="completeTodo(${task.id})">Done</button>`
     }
-
 )}
 
 function completeTodo(id) {
-
     console.log(`you clicked on complete todo with id ${id}`)
-
 }
 
 function deleteTodo(id) {
 
 }
-
-// initializes the first todos 
 
 addToDom()
