ba01106512de67e1ed2c0b690a430827437d859d
Michael Jackson <mjack9615@gmail.com>
2022-10-30 20:18:40 -0700

    mike_j-javascript-todo

--- code/mike_j/javascript/todo/script.js
+++ code/mike_j/javascript/todo/script.js
@@ -1,40 +1,57 @@
-let todoObject = [
-    {
-        name: "Eat",
-        completed: false,
-        id: 1
-    },
-    {
-        name: "Sleep",
-        completed: true,
-        id: 2
-    }   
-]
+let todoObject = []
 
-let input = document.querySelector("#input")
+let input = document.querySelector("#inputBox")
 let add = document.querySelector("#addBtn")
 let todoList = document.querySelector("#todo-list")
 let doneList = document.querySelector("#done-list")
+let idCounter = todoObject.length
+
+document.getElementById("inputBox").value = ""
 
 add.onclick = function() {
-    todoObject.push(input.value)
+    let inputVal = input.value
+    idCounter++
+    let task = {
+        name: inputVal,
+        completed: false,
+        id: idCounter
+    }
+    todoObject.push(task)
     addToDom()
+    document.getElementById("inputBox").value = ""
 }
 
 function addToDom(){
-    let todoList = document.querySelector("#todo-list")
-
+    todoList.innerHTML = ""
     todoObject.forEach(task => {
-        todoList.innerHTML += `<p>${task.name}</p> <button onclick="completeTodo(${task.id})">Done</button>`
-    }
-)}
+        if (task.completed === false) {
+            todoList.innerHTML += `<p>${task.name}</p><button onclick="completeTodo(${task.id})">Done</button>`
+        }
+        else if (task.completed === true)
+            doneList.innerHTML += `<p>${task.name}</p><button onclick="deleteTodo(${task.id})">Delete</button>`
+        }
+    )
+}
 
 function completeTodo(id) {
-    
+    doneList.innerHTML = ""
+    todoObject.forEach(task => {
+        if (task.id === id) {
+            task.completed = !task.completed
+        }
+    })
+    addToDom()   
 }
 
 function deleteTodo(id) {
-
+    doneList.innerHTML = ""
+    todoObject.forEach(task => {
+        if (task.id === id) {
+            todoObject.splice(todoObject.indexOf(id), 1)
+        }
+    })
+    addToDom()
+    console.log(todoObject)
 }
 
 document.addEventListener("keydown", function(event) {
