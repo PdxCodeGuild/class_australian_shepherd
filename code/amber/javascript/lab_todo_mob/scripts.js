d0e84d224184ab08cdf3164266ffec8e1ab2fd12
Amber-DiNenna <amberdinenna@gmail.com>
2022-11-01 19:05:54 -0700

    Complete lab by adjusting addtodom, todo, and deletetodo functions.

--- code/amber/javascript/lab_todo_mob/scripts.js
+++ code/amber/javascript/lab_todo_mob/scripts.js
@@ -10,12 +10,28 @@ let todoObject = [
       id: 2
   },
 ]
+
 let submit = document.querySelector('#submit-btn')
 let todoSection = document.querySelector('#todos-section')
 let completedSection = document.querySelector('#completed-section')
 let inputItem = document.querySelector('#input')
 let idCounter = todoObject.length
-// let removeTodo = document
+// let remove = document.querySelector('#delete-btn')
+// let completeP = document.querySelector('#complete-p')
+
+function addToDom() {
+  todoObject.forEach(inputItem => {
+      if (inputItem.completed === false) {
+        todoSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="completeTodo(${inputItem.id})">Complete</button> <button onclick="deleteTodo(${inputItem.id})">Remove</button>`
+      }
+      else {
+        completedSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="todo(${inputItem.id})">To Do</button> <button onclick="deleteTodo(${inputItem.id})">Remove</button>`
+      }
+  }
+  )
+}
+
+
 submit.onclick = function () {
   let inputItemValue = inputItem.value
   idCounter++
@@ -26,39 +42,75 @@ submit.onclick = function () {
   }
   todoObject.push(person)
   // console.table(todoObject)
+  todoSection.innerHTML = ''
+  completedSection.innerHTML = ''
   addToDom()
   // console.log('you clicked the submit button, this is supposed to be for adding a new item')
 }
-function addToDom() {
+
+function todo(id) {
+  for(let i = 0; i < todoObject.length; i++)
+  {
+    if (todoObject[i].id === id)
+    {
+      todoSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="completeTodo(${inputItem.id})">To Do</button>`
+      todoObject[i].completed = false
+      console.log(todoObject[i])
+    }
+  }
   todoSection.innerHTML = ''
   completedSection.innerHTML = ''
-  todoObject.forEach(task => {
-      if (task.completed === false) {
-          todoSection.innerHTML += `<p>${task.name}</p> <button onclick="completeTodo(${task.id})">complete</button>`
-      }
-      else if (task.completed === true)
-          completedSection.innerHTML += `<p>${task.name}</p> <button onclick="deleteTodo(${task.id})">delete</button>`
-  }
-  )
-}
-function completeTodo(id) {
-  todoObject.forEach(task => {
-      if (task.id === id) {
-          task.completed = !task.completed
-          console.log(task.completed)
-          console.log(`you clicked on complete todo with id ${id}, ${task.id}`)
-      }
-  })
   addToDom()
 }
-function deleteTodo(id) {
-  completedSection.innerHTML -= ''
-  todoObject.forEach(task => {
-      if (task.id == id) {
-          // task.remove(id)
-          console.log(task)
-      }
-  })
+
+function completeTodo(id) {
+  for(let i = 0; i < todoObject.length; i++)
+  {
+    if (todoObject[i].id === id)
+    {
+      completedSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="todo(${inputItem.id})">To Do</button>`
+      todoObject[i].completed = true
+      console.log(todoObject[i])
+    }
+  }
+  todoSection.innerHTML = ''
+  completedSection.innerHTML = ''
+  addToDom()
 }
+
+//
+function deleteTodo(id) {
+  for(let i = 0; i < todoObject.length; i++)
+  {
+    if (todoObject[i].id === id)
+    {
+      // completedSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="todo(${inputItem.id})<button onclick="deleteTodo(${tasks.id})">Remove</button>">To Do</button>`
+      // todoSection.innerHTML += `<p>${inputItem.name}</p> <button onclick="completeTodo(${inputItem.id})<button onclick="deleteTodo(${tasks.id})">Remove</button>">Complete</button>`
+      console.log(todoObject[i])
+      todoObject.splice(i, 1)
+    }
+  }
+  todoSection.innerHTML = ''
+  completedSection.innerHTML = ''
+  addToDom()
+}
+
 // initializes the first todos
 addToDom()
+//
+
+
+
+// KNOWN BUGS:
+
+// COMPLETED LIST READS NaN WHEN EMPTY
+
+// WHEN LIST IS EMPTY AND A NEW ITEM IS SUBMITTED, THE PREVIOUSLY CLEARED ITEMS RETURN TO COMPLETED
+
+// IF YOU DELETE ONE ITEM FROM THE LIST IT DELETES THEM ALL
+
+// INPUT FIELD DOES NOT CLEAR AFTER SUBMISSION
+
+// should make delete button work in todo and redo button in complete
+
+// redo button could just flip the bool... mess with the buttons in addtodom
