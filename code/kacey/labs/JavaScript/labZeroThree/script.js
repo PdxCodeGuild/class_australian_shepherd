61eb59a18af382d0fef44fe6b42a80160a892845
Kacey <kaceylauinger@gmail.com>
2022-11-01 20:30:28 -0700

    Todo Lab Submit

--- code/kacey/labs/JavaScript/labZeroThree/script.js
+++ code/kacey/labs/JavaScript/labZeroThree/script.js
@@ -13,32 +13,28 @@ let todoObject = [
 let submit = document.querySelector("#submit-btn");
 let todoSection = document.querySelector("#todos-section");
 let completedSection = document.querySelector("#completed-section");
-
 submit.onclick = function () {
     let todoInput = document.querySelector("#todo-input").value;
-
     let newTodo = {
         name: `${todoInput}`,
         completed: false,
         id: todoObject.length + 1,
     };
-
     todoObject.push(newTodo);
     console.log(todoObject[0].completed);
     addToDom();
 };
-
 function addToDom() {
     let todoSection = document.querySelector("#todos-section");
     todoSection.innerHTML = "";
     completedSection.innerHTML = "";
     todoObject.forEach((task) => {
         if (task.completed === false) {
-            todoSection.innerHTML += `<p>${task.name}</p> <button onclick="deleteTodo(${task.id})">Delete</button><button onclick="completeTodo(${task.id})">Complete</button>`;
+            todoSection.innerHTML += `<div><button class="todostyle" onclick="completeTodo(${task.id})">${task.name}</button><p class="todostyle"><button onclick="deleteTodo(${task.id})">Delete</button></p></div>`;
         }
         let completedSection = document.querySelector("#completed-section");
         if (task.completed === true) {
-            completedSection.innerHTML += `<p>${task.name}</p> <button onclick="deleteTodo(${task.id})">Delete</button><button onclick="undoComplete(${task.id})">Undo</button>`;
+            completedSection.innerHTML += `<div><button class="todostyle" onclick="undoComplete(${task.id})">${task.name}</button><p class="todostyle"><button onclick="deleteTodo(${task.id})">Delete</button></p></div>`;
         }
     });
 }
@@ -48,20 +44,17 @@ function completeTodo(id) {
     });
     addToDom();
 }
-
 function undoComplete(id) {
     todoObject.forEach((task) => {
         if (task.id === id) task.completed = false;
     });
     addToDom();
 }
-
 function deleteTodo(id) {
     todoObject.forEach((task) => {
         if (task.id === id) todoObject.pop(task);
     });
     addToDom();
 }
-
 // initializes the first todos
 addToDom();
