e7581d5c08147f92cca1b5e4ba71a2e9ea337b6c
Amber-DiNenna <amberdinenna@gmail.com>
2022-11-02 18:40:49 -0700

    Adjust getData function to populate quote of the day to the browser.

--- code/amber/javascript/lab09_quotes/scripts.js
+++ code/amber/javascript/lab09_quotes/scripts.js
@@ -7,21 +7,22 @@ let quote5 = document.querySelector('#quote4')
 
 function getData(){
     axios({
-        method: 'get',
-        url: 'https://favqs.com/api',
+        method: 'GET',
+        url: 'https://favqs.com/api/qotd',
         params: {
-          id: 4,
-          author: "Albert Einstein",
-          body: "Make everything as simple as possible, but not simpler.",
+          "id": 4,
+          "author": "Albert Einstein",
+          "body": "Make everything as simple as possible, but not simpler.",
         }
     }).then((response) => {
-        let dataArray = response
-        console.log(dataArray)
+        let qotd = response.data.quote
+        console.log(qotd)
         // console.log(dataArray)
         // console.log(dataArray[0].correct_answer)
         // content.innerHTML = dataArray[0].question
 
-        // answer1.innerHTML = dataArray[0].correct_answer
+        quote1.innerHTML = qotd.author
+        quote2.innerHTML = qotd.body
         // answer2.innerHTML = dataArray[0].incorrect_answers[0]
         // answer3.innerHTML = dataArray[0].incorrect_answers[1]
         // answer4.innerHTML = dataArray[0].incorrect_answers[2]
