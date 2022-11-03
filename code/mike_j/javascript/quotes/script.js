a276c0d09d948ea3d0c00dae32eb411f74f4fc36
mjack9615 <mjack9615@gmail.com>
2022-11-02 21:04:39 -0700

    mike_j-javascript-quotes

---
+++ code/mike_j/javascript/quotes/script.js
@@ -0,0 +1,18 @@
+// ver.1
+
+// axios({
+//     method: 'get',
+//     url: 'https://favqs.com/api/qotd'
+//     }).then((response) => {
+//     console.log(response.data)
+//     quote1.innerHTML = response.data.quote.body
+//     quote2.innerHTML = response.data.quote.author
+//     })   
+
+//ver.2
+
+let quote1 = document.querySelector('#quote1')
+let quote2 = document.querySelector('#quote2')
+let quote3 = document.querySelector('#quote3')
+let quote4 = document.querySelector('#quote4')
+let quote5 = document.querySelector('#quote4')
