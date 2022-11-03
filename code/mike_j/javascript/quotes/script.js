2571ccd27eceace95302c7c353b89c6c0726fb07
mjack9615 <mjack9615@gmail.com>
2022-11-02 21:20:31 -0700

    mike_j-javascript-quotes

--- code/mike_j/javascript/quotes/script.js
+++ code/mike_j/javascript/quotes/script.js
@@ -1,18 +1,48 @@
 // ver.1
 
+// let quote = document.querySelector('#quote')
+// let author = document.querySelector('#author')
+
 // axios({
 //     method: 'get',
 //     url: 'https://favqs.com/api/qotd'
 //     }).then((response) => {
 //     console.log(response.data)
-//     quote1.innerHTML = response.data.quote.body
-//     quote2.innerHTML = response.data.quote.author
+//     quote.innerHTML = response.data.quote.body
+//     author.innerHTML = response.data.quote.author
 //     })   
 
-//ver.2
+// ver.2
 
-let quote1 = document.querySelector('#quote1')
-let quote2 = document.querySelector('#quote2')
-let quote3 = document.querySelector('#quote3')
-let quote4 = document.querySelector('#quote4')
-let quote5 = document.querySelector('#quote4')
+let search = document.querySelector("#search")
+let submit = document.querySelector("#subBtn")
+let quote = document.querySelector("#quote")
+let author = document.querySelector("#author")
+
+document.getElementById("search").value = ""
+
+submit.onclick = function() {
+    axios({
+        method: 'get',
+        url: 'https://favqs.com/api/quotes',
+        headers: {
+            'Authorization': 'Token token="1ada7bb20e76745351d38e458d0e57b8"'
+        },
+        params: {
+            filter: search.value
+        }
+    }).then((response) => {
+        quote.innerHTML = response.data.quotes[0].body
+        author.innerHTML = response.data.quotes[0].author
+    }).catch((error) => {
+        console.log(error)
+    })
+    document.getElementById("search").value = ""
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("subBtn").click()
+    }
+})
