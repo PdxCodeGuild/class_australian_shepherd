2355a7fe1ffb390bc07357910ea07a388f5d50f5
fmouse01 <mrodriuge75@gmail.com>
2022-11-05 01:43:28 -0700

    complete

---
+++ code/matt_r/java/vueTodo/script.js
@@ -0,0 +1,66 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            message: 'Hello World',
+            inputValue: '',
+            todo: [ 
+                {
+                    name: "Eat",
+                    completed: true,
+                },
+                {
+                    name: 'sleep',
+                    completed: false
+            
+                }
+            
+                
+            ],
+
+        }
+    },
+    methods:{
+        makeNewTodo(){
+            let todoArray = {
+                    name: this.inputValue,
+                    completed: false,
+                }
+            
+            
+            this.todo.push(todoArray)
+            
+            // console.log(this.todo)
+        },
+        changeComplete(item){
+            for (let i = 0; i<this.todo.length; i++){
+                if (this.todo[i].name === item.name){
+                    this.todo[i].completed = !this.todo[i].completed
+        
+                }
+            }
+        },
+        // change(item){
+        //     for (let i = 0; i<this.todo.length; i++){
+        //          if (this.todo[i].name === item.name){
+        //             this.todo[i].pop(item)
+        //             console.log(item)        
+        //          }
+        //      }            
+        // },
+        change(item){
+            for (let i = 0; i<this.todo.length; i++){
+                 if (this.todo[i].name === item.name){
+                    this.todo.pop(item)
+                    console.log(item)
+                    // console.log(this.todo[i])        
+                 }
+             }            
+        },
+
+
+        
+    },
+    setup(){
+        
+    }
+})
