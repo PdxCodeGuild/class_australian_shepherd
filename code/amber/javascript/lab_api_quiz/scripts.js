77f7576aa1b2cb71941015ed3c73961574f715aa
Amber-DiNenna <amberdinenna@gmail.com>
2022-11-07 21:36:54 -0800

    WIP: need to review poke api for axios, etc.

--- code/amber/javascript/lab_api_quiz/scripts.js
+++ code/amber/javascript/lab_api_quiz/scripts.js
@@ -1,84 +1,84 @@
-let startBtn = document.querySelector('#start')
+// let startBtn = document.querySelector('#start')
 
-let questionObject = []
-const allQuestions = []
-let correctAnswer = ''
-
-
-async function getQuestion() {
-    const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple')
-    data = await response.json()
-    questionObject = data['results'][0]
-    correctAnswer = questionObject.correct_answer
-
-    allQuestions[0] = questionObject.correct_answer
-    allQuestions[1] = questionObject.incorrect_answers[0]
-    allQuestions[2] = questionObject.incorrect_answers[1]
-    allQuestions[3] = questionObject.incorrect_answers[2]
-
-    // console.log(allQuestions)
-    allQuestions.sort(() => Math.random() - 0.5)
-    // console.log(allQuestions)
-
-    document.querySelector('#question').innerHTML = questionObject.question
-    // console.log(correctAnswer)
-
-    document.querySelectorAll('.btn').forEach(button => {
-        button.innerHTML = allQuestions[counter]
-        counter++
-    })
-    counter = 0
-}
-
-getQuestion()
-
-let counter = 0
+// let questionObject = []
+// const allQuestions = []
+// let correctAnswer = ''
 
 
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(button.innerHTML)
-        if (button.innerHTML === correctAnswer)
-        {
-            getQuestion()
-            showAnswer('correct')
-            disableClicking()
-            // allQuestions.pop()
+// async function getQuestion() {
+//     const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+//     data = await response.json()
+//     questionObject = data['results'][0]
+//     correctAnswer = questionObject.correct_answer
 
-            console.log(allQuestions)
-        }
-        else{
-            getQuestion()
-            showAnswer('incorrect')
-            disableClicking()
-        }
+//     allQuestions[0] = questionObject.correct_answer
+//     allQuestions[1] = questionObject.incorrect_answers[0]
+//     allQuestions[2] = questionObject.incorrect_answers[1]
+//     allQuestions[3] = questionObject.incorrect_answers[2]
 
-    })
-})
+//     // console.log(allQuestions)
+//     allQuestions.sort(() => Math.random() - 0.5)
+//     // console.log(allQuestions)
 
-let showAnswer = function(result) {
-    if (result === 'correct'){
-        document.querySelector('#annoying-popup').innerHTML = 'You got it! ' + correctAnswer
-        document.querySelector('#annoying-popup').style.color = 'green'
-    }
-    else{
-        document.querySelector('#annoying-popup').innerHTML = 'The correct answer was actually ' + correctAnswer
-        document.querySelector('#annoying-popup').style.color = 'red'
+//     document.querySelector('#question').innerHTML = questionObject.question
+//     // console.log(correctAnswer)
 
-    }
-    document.querySelector('#annoying-popup').style.display = 'block'
-}
+//     document.querySelectorAll('.btn').forEach(button => {
+//         button.innerHTML = allQuestions[counter]
+//         counter++
+//     })
+//     counter = 0
+// }
+
+// getQuestion()
+
+// let counter = 0
 
 
-let disableClicking = function() {
-    document.querySelector('#questions-container').style.pointerEvents  = "none"
-    setTimeout(enableClicking, 1500)
-}
+// document.querySelectorAll('.btn').forEach(button => {
+//     button.addEventListener('click', () => {
+//         console.log(button.innerHTML)
+//         if (button.innerHTML === correctAnswer)
+//         {
+//             getQuestion()
+//             showAnswer('correct')
+//             disableClicking()
+//             // allQuestions.pop()
 
-let enableClicking = function() {
-    document.querySelector('#questions-container').style.pointerEvents  = "auto"
-}
+//             console.log(allQuestions)
+//         }
+//         else{
+//             getQuestion()
+//             showAnswer('incorrect')
+//             disableClicking()
+//         }
+
+//     })
+// })
+
+// let showAnswer = function(result) {
+//     if (result === 'correct'){
+//         document.querySelector('#annoying-popup').innerHTML = 'You got it! ' + correctAnswer
+//         document.querySelector('#annoying-popup').style.color = 'green'
+//     }
+//     else{
+//         document.querySelector('#annoying-popup').innerHTML = 'The correct answer was actually ' + correctAnswer
+//         document.querySelector('#annoying-popup').style.color = 'red'
+
+//     }
+//     document.querySelector('#annoying-popup').style.display = 'block'
+// }
+
+
+// let disableClicking = function() {
+//     document.querySelector('#questions-container').style.pointerEvents  = "none"
+//     setTimeout(enableClicking, 1500)
+// }
+
+// let enableClicking = function() {
+//     document.querySelector('#questions-container').style.pointerEvents  = "auto"
+// }
 
 
 
-// need to clear out answer when new page loads
+// // need to clear out answer when new page loads
