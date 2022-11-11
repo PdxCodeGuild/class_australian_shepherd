8bf53758b01cf473c34dc0085a3e3bed0286387a
mjack9615 <mjack9615@gmail.com>
2022-11-10 21:07:54 -0800

    mike_j-javascript-quiz

--- code/mike_j/javascript/quiz/scripts.js
+++ code/mike_j/javascript/quiz/scripts.js
@@ -2,13 +2,20 @@ const app = Vue.createApp({
     data(){
         return{
             dataArray: [],
+            diff: "",
+            cat: "",
+            question: document.querySelector("#question"),
+            btn1: document.querySelector("#btn1"),
+            btn2: document.querySelector("#btn2"),
+            btn3: document.querySelector("#btn3"),
+            btn4: document.querySelector("#btn4")
         }
     },
     methods:{
         getData(){
             let amount = 10
-            let category = 15
-            let difficulty = "easy"
+            let category = this.cat
+            difficulty = this.diff
             let type = "multiple"
             axios({
                 method: 'get',
@@ -22,6 +29,11 @@ const app = Vue.createApp({
             }).then((response) => {
                 let dataArray = response.data
                 console.log(dataArray)
+                question.innerHTML = dataArray.results[0].question
+                btn1.innerHTML = dataArray.results[0].correct_answer
+                btn2.innerHTML = dataArray.results[0].incorrect_answers[0]
+                btn3.innerHTML = dataArray.results[0].incorrect_answers[1]
+                btn4.innerHTML = dataArray.results[0].incorrect_answers[2]
             })
         }
     }
