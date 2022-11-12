f395a3eaa443668f4027a93c1a4a5cc4c2295693
Tracey Stancliff <mjack9615@gmail.com>
2022-11-11 16:12:51 -0800

    mike_j-javascript-quiz

--- code/mike_j/javascript/quiz/scripts.js
+++ code/mike_j/javascript/quiz/scripts.js
@@ -16,7 +16,7 @@ const app = Vue.createApp({
             difficulty = this.diff
             let type = "multiple"
             axios({
-                method: 'get',
+                method: "get",
                 url: `https://opentdb.com/api.php?`,
                 params: {
                     amount: amount,
@@ -28,42 +28,41 @@ const app = Vue.createApp({
                 let data = response.data
                 questionObject = data["results"][0]
                 correctAnswer = questionObject.correct_answer
-                console.log(correctAnswer)
-                document.querySelector('#question').innerHTML = questionObject.question
+                document.querySelector("#question").innerHTML = questionObject.question
                 this.allQuestions[0] = questionObject.correct_answer
                 this.allQuestions[1] = questionObject.incorrect_answers[0]
                 this.allQuestions[2] = questionObject.incorrect_answers[1]
                 this.allQuestions[3] = questionObject.incorrect_answers[2]
                 this.allQuestions.sort(() => 0.5 - Math.random())    
-                document.querySelectorAll('.btn').forEach(button => {
+                document.querySelectorAll(".btn").forEach(button => {
                     button.innerHTML = this.allQuestions[this.counter]
                     this.counter++
                 })
 
                 this.counter = 0
+                document.querySelector("#result").innerHTML = ""
 
-                document.querySelectorAll('.btn').forEach(button => {
-                    button.addEventListener('click', () => {
+                document.querySelectorAll(".btn").forEach(button => {
+                    button.addEventListener("click", () => {
                         console.log(button.innerHTML)
                         if (button.innerHTML === correctAnswer){
-                            showAnswer('correct')
+                            showAnswer("correct")
                         }
                         else{
-                            showAnswer('incorrect')
+                            showAnswer("incorrect")
                         }     
                     })
                 })
-                
+
                 let showAnswer = function(result) {
-                    if (result === 'correct'){
-                        document.querySelector('#result').innerHTML = 'You got it! ' + correctAnswer
-                        document.querySelector('#result').style.color = 'green'
+                    if (result === "correct"){
+                        document.querySelector("#result").innerHTML = "You got it! " + correctAnswer + "."
+                        document.querySelector("#result").style.color = "green"
                     }
                     else{
-                        document.querySelector('#result').innerHTML = 'The correct answer was actually ' + correctAnswer
-                        document.querySelector('#result').style.color = 'red'       
+                        document.querySelector("#result").innerHTML = "The correct answer was actually " + correctAnswer + "."
+                        document.querySelector("#result").style.color = "red"       
                     }
-                    document.querySelector('#result').style.display = 'block'
                 }
             }
             )},        
